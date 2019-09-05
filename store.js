c7a0abea6fa6c1727bfddadbc514dbc08a4a34eb
blomqma <markus.blomqvist@anders.fi>
2019-09-05 18:02:31 +0300

    Add redux dev tools

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 
-export const initStore = () => {
-  return createStore(rootReducer, applyMiddleware(thunk));
-};
+export const initStore = () =>
+  createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
