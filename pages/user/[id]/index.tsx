c27ebfda82e227656bb10236548d3d3b48bba674
blomqma <markus.blomqvist@anders.fi>
2019-09-06 11:47:09 +0300

    Update tsconfig.json and example dyamic routing usage

---
+++ pages/user/[id]/index.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Header, Layout } from '../../../components';
+
+// Navigate dynamic URLs like this
+export default () => {
+  const router = useRouter();
+
+  const id1 = 1;
+  const id2 = 2;
+
+  return (
+    <Layout title="User">
+      <Header>User {router.query.id}</Header>
+      {/* Link to other dynamic pages like this */}
+      <Link href="/user/[id]" as={`/user/${id1}`}>
+        <a>user 1</a>
+      </Link>
+      <Link href="/user/[id]" as={`/user/${id2}`}>
+        <a>user 2</a>
+      </Link>
+    </Layout>
+  );
+};
