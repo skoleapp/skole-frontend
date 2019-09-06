8f69b0c899023423c9ba6bb935022cb1f014ddf3
blomqma <markus.blomqvist@anders.fi>
2019-09-06 14:32:26 +0300

    Remove data fetch example

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,7 @@
-import withRedux from 'next-redux-wrapper';
 import App from 'next/app';
 import React, { useEffect } from 'react';
 import { Provider, useDispatch } from 'react-redux';
-import { getUser, initStore } from '../redux';
-
-export default withRedux(initStore, { debug: false })(
-  class StoreProvider extends App {
-    static async getInitialProps({ Component, ctx }) {
-      return {
-        pageProps: {
-          ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})
-        }
-      };
-    }
-
-    render() {
-      const { Component, pageProps, store } = this.props;
-
-      return (
-        <Provider store={store}>
-          <UserProvider {...pageProps} Component={Component} />
-        </Provider>
-      );
-    }
-  }
-);
+import { getUser } from '../redux';
 
 // Here we can use redux actions - load user automatically
 const UserProvider = ({ pageProps, Component }) => {
@@ -37,3 +14,25 @@ const UserProvider = ({ pageProps, Component }) => {
 
   return <Component {...pageProps} />;
 };
+
+class StoreProvider extends App {
+  static async getInitialProps({ Component, ctx }) {
+    return {
+      pageProps: {
+        ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {})
+      }
+    };
+  }
+
+  render() {
+    const { Component, pageProps, store } = this.props;
+
+    return (
+      <Provider store={store}>
+        <UserProvider {...pageProps} Component={Component} />
+      </Provider>
+    );
+  }
+}
+
+export default withRedux(initStore, { debug: false })(StoreProvider);
