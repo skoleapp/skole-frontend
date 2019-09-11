733cd25507bce823998a0f642bba9a6af6da7457
werneriaa <werneri.aarnio@gmail.com>
2019-09-11 13:44:22 +0300

    Loadingscreen component added

--- components/layout/LoadingScreen.tsx
+++ components/layout/LoadingScreen.tsx
@@ -1,3 +1,15 @@
 import React from 'react';
+import { css } from '@emotion/core';
+import ClipLoader from 'react-spinners/ClipLoader';
 
-export const LoadingScreen: React.FC = () => <h3>Loading...</h3>;
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: black;
+`;
+
+export const LoadingScreen: React.FC = () => (
+  <div>
+    <ClipLoader css={override} sizeUnit={'px'} size={35} color={'var(--primary)'} />
+  </div>
+);
