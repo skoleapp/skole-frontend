508f79c1bafb85e355a7f65f08cb5c53d04cdf03
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-09-18 02:05:58 +0300

    FIx linting errors

--- components/atoms/forms/Form.tsx
+++ components/atoms/forms/Form.tsx
@@ -2,8 +2,11 @@ import { Form as FormikForm, FormikProps } from 'formik';
 import React from 'react';
 import { LoginFormValues, RegisterFormValues } from '../../../interfaces';
 
-interface Props extends FormikProps<LoginFormValues | RegisterFormValues> {}
-
-export const Form: React.ComponentType<Props> = ({ handleSubmit, children }) => (
-  <FormikForm onKeyDown={e => e.key === 'Enter' && handleSubmit()}>{children}</FormikForm>
+export const Form: React.ComponentType<FormikProps<LoginFormValues | RegisterFormValues>> = ({
+  handleSubmit,
+  children
+}) => (
+  <FormikForm onKeyDown={(e): false | void => e.key === 'Enter' && handleSubmit()}>
+    {children}
+  </FormikForm>
 );
