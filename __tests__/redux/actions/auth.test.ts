704404db1d21ae9d1c54e5c0304d3dc23f0c0f4d
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-09-22 00:52:16 +0300

    Configure moxios

--- __tests__/redux/actions/auth.test.ts
+++ __tests__/redux/actions/auth.test.ts
@@ -1,12 +1,21 @@
+import moxios from 'moxios';
 import { useDispatch } from 'react-redux';
 import { initialAuthState, initStore, setToken } from '../../../redux';
 
 describe('auth actions', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
   const store = initStore();
   const dispatch = useDispatch();
 
   describe('set token action', () => {
-    it('dispatches action with type `SET_TOKEN` and token as payload', () => {
+    it('sets the token in the store', () => {
       dispatch(setToken('token'));
       const state = store.getState();
 
@@ -18,6 +27,8 @@ describe('auth actions', () => {
       expect(state).toEqual(expectedState);
     });
   });
+
+  it('refreshes the token', () => {});
 
   // TODO: Implement tests for other action creators...
 });
