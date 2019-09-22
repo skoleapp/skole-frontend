044f4ea7533dfeb6c4cdc8c6ee741300fdbf7a84
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-09-22 14:28:18 +0300

    Update logout page

--- components/atoms/MenuListItem.tsx
+++ components/atoms/MenuListItem.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import { logout, toggleMenu } from '../../redux';
+import { toggleMenu } from '../../redux';
 import { Anchor } from './Anchor';
 
 const StyledMenuListItem = styled.li`
@@ -29,17 +29,13 @@ interface Props {
 export const MenuListItem: React.FC<Props> = ({ href, children }) => {
   const dispatch = useDispatch();
 
-  const onClick = (href: string): void => {
-    dispatch(toggleMenu(false));
-
-    if (href === '/logout') {
-      dispatch(logout());
-    }
-  };
-
   return (
     <Link href={href}>
-      <StyledMenuListItem onClick={(): void => onClick(href)}>
+      <StyledMenuListItem
+        onClick={(): void => {
+          dispatch(toggleMenu(false));
+        }}
+      >
         <Anchor variant="white">{children}</Anchor>
       </StyledMenuListItem>
     </Link>
