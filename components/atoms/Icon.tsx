cf72d4c9137a7e1ec702099d7e9ffd39bddfb377
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-01 21:03:13 +0300

    Update icon

--- components/atoms/Icon.tsx
+++ components/atoms/Icon.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+type IconVariant = 'white';
+
+interface IconProps {
+  variant?: IconVariant;
+}
+
+const getColorForVariant = (variant?: string): string => {
+  switch (variant) {
+    case 'white':
+      return 'var(--white)';
+    default:
+      return 'var(--black)';
+  }
+};
+
+const StyledIcon = styled.i<IconProps>`
+  color: ${({ variant }): string | undefined => getColorForVariant(variant)};
+`;
 
 interface Props {
   iconName: string;
+  variant?: IconVariant;
 }
 
-export const Icon: React.FC<Props> = ({ iconName }) => <i className={`fas fa-3x fa-${iconName}`} />;
+export const Icon: React.FC<Props> = ({ iconName, variant }) => (
+  <StyledIcon variant={variant} className={`fas fa-3x fa-${iconName}`} />
+);
