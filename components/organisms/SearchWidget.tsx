a724f0781e7fa1b24694e7d05d57de2d5c5243ec
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-03 00:27:13 +0300

    Add hook for widgets and search widget

--- components/organisms/SearchWidget.tsx
+++ components/organisms/SearchWidget.tsx
@@ -1,12 +1,69 @@
+import { Formik } from 'formik';
+import Router from 'next/router';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { SearchFormProps, State, WidgetOpenProps } from '../../interfaces';
+import { closeWidgets, toggleSearchInput } from '../../redux';
 import { Icon } from '../atoms/Icon';
+import { useWidget } from '../hooks';
+import { SearchInputSection } from '../molecules';
 
-const StyledHomeButton = styled.div``;
+export const initialValues = {
+  search: ''
+};
+
+const StyledSearchWidget = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const SearchInput = styled.div<WidgetOpenProps>`
+  position: absolute;
+  height: auto;
+  width: auto;
+  top: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--primary);
+  visibility: ${({ open }): string => (open ? 'visible' : 'hidden')};
+  border-radius: 0 0 var(--border-radius) var(--border-radius);
+  padding: 1rem 0.5rem;
+  border: var(--black-border);
+  border-style: none solid solid solid;
+`;
 
 // TODO: Implement a proper search widget
-export const SearchWidget: React.FC = () => (
-  <StyledHomeButton onClick={(): void => alert('I am useless!')}>
-    <Icon iconName="search" variant="white" iconSize="2" />
-  </StyledHomeButton>
-);
+export const SearchWidget: React.FC = () => {
+  const { searchInputOpen } = useSelector((state: State) => state.ui);
+  const dispatch = useDispatch();
+  const node = useWidget('search-input');
+
+  const onIconClick = (): void => {
+    if (searchInputOpen) {
+      dispatch(toggleSearchInput(false));
+    }
+  };
+
+  const onSubmit = (values: SearchFormProps): void => {
+    const { search } = values;
+    Router.push({ pathname: '/search-courses', query: { search } });
+    dispatch(closeWidgets());
+  };
+
+  return (
+    <StyledSearchWidget
+      onClick={(): void => {
+        !searchInputOpen && dispatch(toggleSearchInput(true));
+      }}
+      ref={node}
+    >
+      <Icon iconName="search" variant="white" iconSize="2" onClick={onIconClick} />
+      <SearchInput open={searchInputOpen}>
+        <Formik component={SearchInputSection} onSubmit={onSubmit} initialValues={initialValues} />
+      </SearchInput>
+    </StyledSearchWidget>
+  );
+};
--- redux/reducers/ui.ts
+++ redux/reducers/ui.ts
@@ -1,10 +1,16 @@
 import { AnyAction } from 'redux';
 import { UIState } from '../../interfaces';
-import { CLOSE_WIDGETS, TOGGLE_DESKTOP_MENU_DROPDOWN, TOGGLE_MENU } from '../actions/types';
+import {
+  CLOSE_WIDGETS,
+  TOGGLE_DESKTOP_MENU_DROPDOWN,
+  TOGGLE_MENU,
+  TOGGLE_SEARCH_INPUT
+} from '../actions/types';
 
 const initialState: UIState = {
   menuOpen: false,
-  desktopMenuDropdownOpen: false
+  authMenuOpen: false,
+  searchInputOpen: false
 };
 
 export default (state = initialState, action: AnyAction): UIState => {
@@ -12,9 +18,11 @@ export default (state = initialState, action: AnyAction): UIState => {
     case TOGGLE_MENU:
       return { ...state, menuOpen: action.payload };
     case TOGGLE_DESKTOP_MENU_DROPDOWN:
-      return { ...state, desktopMenuDropdownOpen: action.payload };
+      return { ...state, authMenuOpen: action.payload };
     case CLOSE_WIDGETS:
       return { ...initialState };
+    case TOGGLE_SEARCH_INPUT:
+      return { ...state, searchInputOpen: action.payload };
     default:
       return state;
   }
