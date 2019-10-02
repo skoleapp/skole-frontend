29b0884f9cd18280a95b7abfc239b5ad27dff93b
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-03 00:24:48 +0300

    Add useWidget hook

--- components/hooks/index.ts
+++ components/hooks/index.ts
@@ -1,1 +1,4 @@
+export { useAccount } from './useAccount';
 export { useMobileBreakPoint } from './useMobileBreakPoint';
+export { useSearch } from './useSearch';
+export { useWidget } from './useWidget';
