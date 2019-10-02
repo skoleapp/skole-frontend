d4deb9897071bea65df9103aceb4630754f5ba5e
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-03 00:29:02 +0300

    Update landing page

--- components/organisms/LandingPage.tsx
+++ components/organisms/LandingPage.tsx
@@ -1,33 +1,20 @@
-import { Formik } from 'formik';
-import Router from 'next/router';
 import React from 'react';
 import styled from 'styled-components';
-import { SearchFormProps } from '../../interfaces';
 import { actionShortcuts, schoolShortcuts } from '../../static';
 import { LogoHeader } from '../atoms';
-import { SearchInputSection, ShortcutsSection } from '../molecules';
-
-export const initialValues = {
-  search: ''
-};
+import { ShortcutsSection } from '../molecules';
 
 const StyledLandingPage = styled.div`
   margin-top: 2rem;
 `;
 
 const LandingpageShortcuts = styled.div`
-  margin-top: 8rem;
+  margin-top: 4rem;
 `;
-
-const onSubmit = (values: SearchFormProps): void => {
-  const { search } = values;
-  Router.push({ pathname: '/search-courses', query: { search } });
-};
 
 export const LandingPage: React.FC = () => (
   <StyledLandingPage>
     <LogoHeader />
-    <Formik component={SearchInputSection} onSubmit={onSubmit} initialValues={initialValues} />
     <LandingpageShortcuts>
       <ShortcutsSection shortcuts={schoolShortcuts} title="Where do you study?" />
       <ShortcutsSection shortcuts={actionShortcuts} title="What would you like to do?" />
