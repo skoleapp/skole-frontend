61a23518e60d77fd850c5ff943b7013d229f1f1b
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-04 00:25:14 +0300

    Remove remaining search things

--- components/containers/ErrorBoundary.tsx
+++ components/containers/ErrorBoundary.tsx
@@ -5,14 +5,9 @@ import { ErrorPage } from '../organisms';
 
 export const ErrorBoundary: React.FC = ({ children }) => {
   const authError = useSelector((state: State) => state.auth.errors);
-  const searchError = useSelector((state: State) => state.search.errors);
 
   if (authError && authError.serverNotFound) {
     return <ErrorPage error={authError.serverNotFound} />;
-  }
-
-  if (searchError && searchError.serverNotFound) {
-    return <ErrorPage error={searchError.serverNotFound} />;
   }
 
   return <>{children}</>;
