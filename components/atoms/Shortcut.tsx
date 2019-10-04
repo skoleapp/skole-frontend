1f81454ad94b9f5d6933e27c247c1b7b9203572f
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-04 19:21:00 +0300

    Update shortcut to use Card

--- components/atoms/Shortcut.tsx
+++ components/atoms/Shortcut.tsx
@@ -2,39 +2,27 @@ import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 import { ShortcutProps } from '../../interfaces';
-import { Column } from '../containers';
+import { Card } from './Card';
 import { Icon } from './Icon';
 
-const StyledShortcut = styled.div`
-  margin: 0.5rem;
-  padding: 0.5rem;
+const StyledCard = styled(Card)`
   height: 14rem;
   width: 14rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: var(--border-radius);
-  border: var(--black-border)
-  color: var(--black);
   font-size: 1.5rem;
-  background: var(--white);
-  box-shadow: var(--box-shadow);
+  margin: 1rem;
 
   &:hover {
     transition: var(--transition);
     transform: var(--scale);
-    border-color: var(--primary);
     cursor: pointer;
   }
 `;
 
 export const Shortcut: React.FC<ShortcutProps> = ({ text, iconName, href }) => (
   <Link href={href}>
-    <StyledShortcut>
-      <Column>
-        <Icon iconName={iconName} iconSize="3" />
-        <p>{text}</p>
-      </Column>
-    </StyledShortcut>
+    <StyledCard>
+      <Icon iconName={iconName} iconSize="3" />
+      <p>{text}</p>
+    </StyledCard>
   </Link>
 );
