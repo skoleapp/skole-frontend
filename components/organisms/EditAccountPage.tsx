59dcc43a8341760d82c2af2e9d48b02dada21ca3
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-04 19:26:13 +0300

    Add form link section to forms

--- components/organisms/EditAccountPage.tsx
+++ components/organisms/EditAccountPage.tsx
@@ -1,8 +1,7 @@
 import { Formik } from 'formik';
-import Link from 'next/link';
 import React from 'react';
 import { User } from '../../interfaces';
-import { Button, H1 } from '../atoms';
+import { Card, H1 } from '../atoms';
 import { EditAccountForm } from '../molecules';
 
 interface Props {
@@ -19,12 +18,9 @@ export const EditAccountPage: React.FC<Props> = ({ initialValues }) => {
   const onSubmit = (values: User): void => console.log('Submit!', values);
 
   return (
-    <>
+    <Card>
       <H1>Edit Account</H1>
       <Formik component={EditAccountForm} initialValues={initialValues} onSubmit={onSubmit} />
-      <Link href="/account">
-        <Button>back to account</Button>
-      </Link>
-    </>
+    </Card>
   );
 };
