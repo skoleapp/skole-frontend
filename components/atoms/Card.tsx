4e0a5a9e7d5ab837c987782ecf923d0b5a8a6aff
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-04 19:19:57 +0300

    Add card atom back

---
+++ components/atoms/Card.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { XS } from '../../static';
+
+export const Card = styled.div`
+  border: var(--black-border);
+  background: var(--white);
+  border-radius: var(--border-radius);
+  padding: 1rem;
+  margin: 0 1rem;
+  box-shadow: var(--box-shadow);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 25rem;
+
+  @media screen and (min-width: ${XS}) {
+    margin: 0 auto;
+  }
+`;
