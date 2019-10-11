b4162ad8a3b527044d552ac97d9288a602a4618c
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-11 20:10:14 +0300

    Update account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,52 +1,18 @@
-import fetch from 'isomorphic-unfetch';
 import { NextPage } from 'next';
-import nextCookie from 'next-cookies';
-import Router from 'next/router';
 import React from 'react';
 import { AccountPage, MainLayout, PrivatePage } from '../components';
-import { getHost, withAuthSync } from '../utils';
+import { useAccount } from '../components/hooks';
+import { withAuthSync } from '../utils';
 
-const Account: NextPage = () => {
-  // const [user, loading] = useAccount();
-
-  // if (loading) {
-  //   return <LoadingScreen loadingText="Loading account details..." />;
-  // }
-
-  return (
-    <MainLayout title="Account">
-      <PrivatePage component={AccountPage} />
-    </MainLayout>
-  );
-};
+const Account: NextPage = () => (
+  <MainLayout title="Account">
+    <PrivatePage component={AccountPage} />
+  </MainLayout>
+);
 
 Account.getInitialProps = async ctx => {
-  const { token } = nextCookie(ctx);
-  const apiUrl = getHost(ctx.req) + '/account';
-
-  const redirectOnError = () =>
-    typeof window !== 'undefined'
-      ? Router.push('/login')
-      : ctx.res && ctx.res.writeHead(302, { Location: '/login' }).end();
-
-  try {
-    const res = await fetch(apiUrl, {
-      credentials: 'include',
-      headers: {
-        Authorization: JSON.stringify({ token })
-      }
-    });
-
-    if (res.ok) {
-      const js = await res.json();
-      console.log('js', js);
-      return js;
-    } else {
-      return await redirectOnError();
-    }
-  } catch (error) {
-    return redirectOnError();
-  }
+  const data = await useAccount(ctx);
+  return { data };
 };
 
 export default withAuthSync(Account);
