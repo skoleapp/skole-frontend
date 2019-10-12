aa42d3ac33d54c6d841fbccdc5b3dd6cdfd116b1
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-12 18:27:59 +0300

    Remove more redux stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
 import { ClientContext, GraphQLClient } from 'graphql-hooks';
 import { NextPage, NextPageContext } from 'next';
-import withRedux from 'next-redux-wrapper';
 import { AppContextType } from 'next/dist/next-server/lib/utils';
 import Router, { Router as RouterType } from 'next/router';
 import React, { useState } from 'react';
-import { Provider } from 'react-redux';
-import { Store } from 'redux';
 import { LoadingScreen } from '../components/layout';
 import withGraphQLClient from '../lib/with-graphql-client';
-import { initStore } from '../redux';
 import '../styles';
 
 interface StatelessPage<P = {}> extends React.FC<P> {
@@ -18,10 +14,9 @@ interface Props {
   Component: NextPage<any>; // eslint-disable-line
   pageProps: NextPageContext;
   graphQLClient: GraphQLClient;
-  store: Store;
 }
 
-const App: StatelessPage<Props> = ({ Component, pageProps, graphQLClient, store }) => {
+const App: StatelessPage<Props> = ({ Component, pageProps, graphQLClient }) => {
   const [redirect, setRedirect] = useState(false);
 
   Router.events.on('routeChangeStart', () => setRedirect(true));
@@ -34,11 +29,9 @@ const App: StatelessPage<Props> = ({ Component, pageProps, graphQLClient, store 
 
   return (
     <ClientContext.Provider value={graphQLClient}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <Component {...pageProps} />
     </ClientContext.Provider>
   );
 };
 
-export default withGraphQLClient(withRedux(initStore, { debug: false })(App));
+export default withGraphQLClient(App);
