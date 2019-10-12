74dcb4928a6c919b71ac82d3513b90b884253a1c
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-12 18:26:26 +0300

    Remove redux

--- redux/reducers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './auth';
-export * from './rootReducer';
-export * from './ui';
--- redux/reducers/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './auth';
-import uiReducer from './ui';
-
-export const rootReducer = combineReducers({
-  auth: authReducer,
-  ui: uiReducer
-});
