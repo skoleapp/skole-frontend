4a7484e29f20144e1638586754751ca2e5bee921
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-13 17:25:54 +0300

    Remove private page

--- components/containers/index.ts
+++ components/containers/index.ts
@@ -2,5 +2,4 @@ export { Column } from './Column';
 export { Container } from './Container';
 export { ErrorBoundary } from './ErrorBoundary';
 export { IntersectingWrapper } from './IntersectingWrapper';
-export { PrivatePage } from './PrivatePage';
 export { Row } from './Row';
