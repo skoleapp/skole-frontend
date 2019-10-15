2c577f7d7c9a3e58458d316b226a05c83051a21f
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-15 23:58:53 +0300

    Add getUser and getUserMe

---
+++ lib/getUserMe.ts
@@ -0,0 +1,27 @@
+import ApolloClient from 'apollo-client';
+import gql from 'graphql-tag';
+
+export const getUserMe = async (apolloClient: ApolloClient<any>) => {
+  try {
+    const { data } = await apolloClient.query({
+      query: gql`
+        query {
+          userMe {
+            id
+            username
+            title
+            bio
+            points
+            created
+            email
+            language
+          }
+        }
+      `
+    });
+
+    return data.userMe;
+  } catch (error) {
+    console.log(error);
+  }
+};
