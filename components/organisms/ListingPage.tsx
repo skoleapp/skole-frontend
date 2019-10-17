29272f394b6291259f45b5433c573861edd01dac
Wiljam <wiljam.peltomaa@congrid.fi>
2019-10-17 14:33:59 +0300

    Added materialUi

--- components/organisms/ListingPage.tsx
+++ components/organisms/ListingPage.tsx
@@ -3,6 +3,12 @@ import * as R from 'ramda';
 import { Anchor } from '../atoms';
 import Router from 'next/router';
 
+import { withStyles } from '@material-ui/core/styles';
+import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+
 const schools = [
   {
     id: 'Turun yliopisto',
@@ -26,8 +32,8 @@ const schools = [
       { id: 'Turun kauppakorkeakoulu', facility: [] },
       { id: 'Yhteiskuntatieteellinen tiedekunta', facility: [] }
     ]
-  },
-  { id: 'Åbo Akademi', faculty: [{ id: 'Department of Gender Studies', facility: [] }] },
+  }
+  /*   { id: 'Åbo Akademi', faculty: [{ id: 'Department of Gender Studies', facility: [] }] },
   { id: 'Aalto-yliopisto', faculty: [{ id: 'lute' }, { id: 'kauppis' }] },
   { id: 'Helsingin yliopisto', faculty: [{ id: 'lute' }, { id: 'kauppis' }] },
   {
@@ -39,8 +45,52 @@ const schools = [
   {
     id: 'Lappeenrannan–Lahden teknillinen yliopisto',
     faculty: [{ id: 'lute' }, { id: 'kauppis' }]
-  }
+  } */
 ];
+
+const ExpansionPanel = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0
+    },
+    '&:before': {
+      display: 'none'
+    },
+    '&$expanded': {
+      margin: 'auto'
+    }
+  },
+  expanded: {}
+})(MuiExpansionPanel);
+
+const ExpansionPanelSummary = withStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    borderBottom: '1px solid rgba(0, 0, 0, .125)',
+    marginBottom: -1,
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56
+    }
+  },
+  content: {
+    '&$expanded': {
+      margin: '12px 0'
+    }
+  },
+  expanded: {
+    backgroundColor: ''
+  }
+})(MuiExpansionPanelSummary);
+
+const ExpansionPanelDetails = withStyles({
+  root: {
+    padding: '0px 0px 0px 24px',
+    textAlign: 'left'
+  }
+})(MuiExpansionPanelDetails);
 
 interface Props {
   schoolType: string;
@@ -74,46 +124,32 @@ const FacultyRow: React.FC<FacultyRowProps> = ({
   const isExpanded = selectedFaculty === index ? true : false;
 
   const facilities = R.prop('facility', faculty);
+  console.log(facilities);
 
   return (
-    <div
+    <ExpansionPanel
+      square
+      expanded={selectedFaculty === index}
       onClick={e => handleFacultySelection(index, e)}
-      style={{
-        backgroundColor: index % 2 === 0 ? '#f0f0f0' : '#f9f9f9',
-        padding: '20px 10px',
-        boxSizing: 'border-box',
-        textAlign: 'left',
-        userSelect: 'none',
-        cursor: 'pointer'
-      }}
     >
-      <div>{R.prop('id', faculty)}</div>
-      {isExpanded ? (
-        <div
-          style={{
-            margin: 20,
-            display: 'flex',
-            flexDirection: 'column',
-            border: '0 none black',
-            borderLeft: '1px groove',
-            borderBottom: '1px groove'
-          }}
-        >
-          <ul>
-            {facilities.map((facility: any, index: number) => (
-              <li style={{ margin: 10 }} key={index}>
-                <Anchor
-                  href={R.prop('href', facility)}
-                  onClick={e => handleFacilityClick(e, R.prop('href', facility))}
-                >
-                  {R.prop('id', facility)}
-                </Anchor>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : null}
-    </div>
+      <ExpansionPanelSummary aria-controls="panel1d-content" id="panel1d-header">
+        <Typography>{R.prop('id', faculty)}</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <ul>
+          {facilities.map((facility: any, index: number) => (
+            <li style={{ margin: 10 }} key={index}>
+              <Anchor
+                href={R.prop('href', facility)}
+                onClick={e => handleFacilityClick(e, R.prop('href', facility))}
+              >
+                {R.prop('id', facility)}
+              </Anchor>
+            </li>
+          ))}
+        </ul>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   );
 };
 
@@ -141,28 +177,16 @@ const SchoolRow: React.FC<SchoolRowProps> = ({
   const faculties = R.prop('faculty', school);
 
   return (
-    <div
+    <ExpansionPanel
+      square
+      expanded={selectedSchool === index}
       onClick={() => handleSchoolSelection(index)}
-      style={{
-        backgroundColor: index % 2 === 0 ? '#f0f0f0' : '#f9f9f9',
-        padding: '20px',
-        textAlign: 'left',
-        userSelect: 'none',
-        cursor: 'pointer'
-      }}
     >
-      <div>{R.prop('id', school)}</div>
-      {isExpanded ? (
-        <div
-          style={{
-            margin: '20px 10px',
-            display: 'flex',
-            flexDirection: 'column',
-            border: '0 none black',
-            borderLeft: '1px groove',
-            borderBottom: '1px groove'
-          }}
-        >
+      <ExpansionPanelSummary aria-controls="panel1d-content" id="panel1d-header">
+        <Typography>{R.prop('id', school)}</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography style={{ width: '100%' }}>
           {faculties.map((faculty: any, index: number) => (
             <FacultyRow
               key={index}
@@ -172,9 +196,9 @@ const SchoolRow: React.FC<SchoolRowProps> = ({
               selectedFaculty={selectedFaculty}
             />
           ))}
-        </div>
-      ) : null}
-    </div>
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   );
 };
 
