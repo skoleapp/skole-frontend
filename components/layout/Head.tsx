bbecb2f5aa182687f8ff9675a034f79325c1cab3
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-18 15:16:56 +0300

    Update document file

--- components/layout/Head.tsx
+++ components/layout/Head.tsx
@@ -8,10 +8,5 @@ interface Props {
 export const HeadComponent: React.FC<Props> = ({ title }) => (
   <Head>
     <title>{`Skole | ${title}`}</title>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css"
-    />
   </Head>
 );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
-import Document, { DocumentContext } from 'next/document';
+import Document, { DocumentContext, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
+import { Head } from '../components/layout';
 
 // eslint-disable-next-line
 export default class SkoleDocument extends Document {
@@ -30,5 +31,24 @@ export default class SkoleDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
