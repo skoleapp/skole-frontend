07b426581343d1f61e952a26f3066a7dc732b178
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-20 01:25:07 +0300

    Remove error boundary

--- components/containers/index.ts
+++ components/containers/index.ts
@@ -1,5 +1,4 @@
 export { Column } from './Column';
 export { Container } from './Container';
-export { ErrorBoundary } from './ErrorBoundary';
 export { IntersectingWrapper } from './IntersectingWrapper';
 export { Row } from './Row';
