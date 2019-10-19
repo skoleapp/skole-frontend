2519198a875dcfb24fccbb9f6454061171f645f2
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-20 00:26:51 +0300

    Update logout

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,51 +1,31 @@
-import cookie from 'cookie';
 import { NextPage } from 'next';
 import Link from 'next/link';
-import React, { useEffect } from 'react';
-import { useApolloClient } from 'react-apollo';
+import React from 'react';
 import { Anchor, Button, Card, H2, MainLayout } from '../components';
-import { withApollo } from '../lib';
+import { SkoleContext } from '../interfaces';
+import { redirect, withAuthSync } from '../lib';
 
-const LogoutPage: NextPage = () => {
-  const apolloClient = useApolloClient();
+const LogoutPage: NextPage = () => (
+  <MainLayout title="Login">
+    <Card>
+      <H2>You have been logged out!</H2>
+      <Link href="/login">
+        <Anchor>
+          <Button>log back in</Button>
+        </Anchor>
+      </Link>
+    </Card>
+  </MainLayout>
+);
 
-  useEffect(() => {
-    document.cookie = cookie.serialize('token', '', {
-      maxAge: -1 // Expire the cookie immediately
-    });
+LogoutPage.getInitialProps = async (ctx: SkoleContext): Promise<{}> => {
+  const { authenticated } = ctx.store.getState().auth;
 
-    // Force a reload of all the current queries now that the user is
-    // logged in, so we don't accidentally leave any state around.
-    apolloClient.cache.reset();
-  }, []);
+  if (authenticated) {
+    redirect(ctx, '/');
+  }
 
-  return (
-    <MainLayout title="Login">
-      <Card>
-        <H2>You have been logged out!</H2>
-        <Link href="/login">
-          <Anchor>
-            <Button>log back in</Button>
-          </Anchor>
-        </Link>
-      </Card>
-    </MainLayout>
-  );
+  return {};
 };
 
-// LogoutPage.getInitialProps = async ({ req, store, apolloClient }: SkoleContext): Promise<{}> => {
-//   store.dispatch({ type: LOGOUT }); // Clear store.
-
-//   // Expire the cookie either on server or in the browser.
-//   req
-//     ? req.headers.cookie || ''
-//     : (document.cookie = cookie.serialize('token', '', {
-//         maxAge: -1
-//       }));
-
-//   // Force a reload of all the current queries.
-//   apolloClient.cache.reset();
-//   return {};
-// };
-
-export default withApollo(LogoutPage);
+export default withAuthSync(LogoutPage);
