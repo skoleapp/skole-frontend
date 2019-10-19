cd9a9a7107ccfd9a02fb84a8f8699d49d2b3fcfe
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-20 01:22:35 +0300

    Remove global shortcut interfaces

--- interfaces/ui.ts
+++ interfaces/ui.ts
@@ -4,19 +4,6 @@ export interface WidgetOpenProps {
   open: boolean;
 }
 
-interface Href {
-  pathname: string;
-  query?: {
-    schoolType: string;
-  };
-}
-
-export interface ShortcutProps {
-  text: string;
-  iconName: string;
-  href: Href;
-}
-
 export type Variant = 'white' | 'red' | undefined;
 
 export interface VariantProps {
