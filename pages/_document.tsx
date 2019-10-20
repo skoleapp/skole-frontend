9a55871d62247e1eb6516373d7241d4b9c36c343
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-21 02:13:52 +0300

    Update document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,56 @@
-import Document, { DocumentContext } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
+import Document, { DocumentContext, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
+import flush from 'styled-jsx/server';
 
-// eslint-disable-next-line
 export default class SkoleDocument extends Document {
-  // eslint-disable-next-line
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
+    const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     try {
-      // eslint-disable-next-line
       ctx.renderPage = () =>
         originalRenderPage({
-          // eslint-disable-next-line
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: (App: any) => (
+            props: any // eslint-disable-line
+          ) => sheet.collectStyles(sheets.collect(<App {...props} />))
         });
 
       const initialProps = await Document.getInitialProps(ctx);
+
       return {
         ...initialProps,
         styles: (
           <>
             {initialProps.styles}
+            {sheets.getStyleElement()}
             {sheet.getStyleElement()}
+            {flush() || null}
           </>
         )
       };
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
