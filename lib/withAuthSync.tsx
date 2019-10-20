6856ff61472cd64c683e89ba18a3ad3168f7fb1b
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-21 02:11:59 +0300

    Update withAuthSync

--- lib/withAuthSync.tsx
+++ lib/withAuthSync.tsx
@@ -2,17 +2,22 @@ import { NextPage } from 'next';
 import { getUserMe } from '../actions';
 import { SkoleContext } from '../interfaces';
 import { withApollo } from './apollo';
+import { getToken } from './getToken';
 
-// This is a wrapper component to automatically update the user in the store.
+/*
+ * This is a wrapper component to automatically update
+ * the user in the store if the user is already authenticated.
+ */
 export const withAuthSync = (WrappedComponent: NextPage) => {
   const Wrapper = (props: any) => <WrappedComponent {...props} />; // eslint-disable-line
 
   Wrapper.getInitialProps = async (ctx: SkoleContext) => {
-    const { store } = ctx;
-    const { authenticated } = store.getState().auth;
+    const { req, store, apolloClient } = ctx;
 
-    if (authenticated) {
-      await store.dispatch(getUserMe(ctx.apolloClient));
+    const token = getToken(req);
+
+    if (token) {
+      await store.dispatch(getUserMe(apolloClient));
     }
 
     const componentProps =
