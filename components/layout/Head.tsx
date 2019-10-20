ca404056ff867ca76c07debfe51fdc5d2ad16cbb
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-20 13:15:44 +0300

    Update meta tag

--- components/layout/Head.tsx
+++ components/layout/Head.tsx
@@ -9,6 +9,9 @@ export const HeadComponent: React.FC<Props> = ({ title }) => (
   <Head>
     <title>{`Skole | ${title}`}</title>
     <meta charSet="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="viewport"
+      content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+    />
   </Head>
 );
