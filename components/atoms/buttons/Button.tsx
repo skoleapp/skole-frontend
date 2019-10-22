a8ffb3b5e517a0d544571d04ab0bfe5d53992558
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-23 01:52:58 +0300

    Update Button

--- components/atoms/buttons/Button.tsx
+++ components/atoms/buttons/Button.tsx
@@ -1,31 +1,7 @@
+import { Button } from '@material-ui/core';
 import styled from 'styled-components';
 
-export const Button = styled.button`
-  cursor: pointer;
-  background var(--primary);
-  border-radius: var(--border-radius);
-  margin: 0.5rem;
-  padding: 0.5rem;
-  color: var(--secondary);
-  width: 100%;
-  max-width: 18.5rem;
-  height: 2.75rem;
-  font-size: 1.05rem;
-  text-transform: uppercase;
-  box-shadow: var(--box-shadow);
-  border: var(--white-border);
-
-  &:hover {
-    transform: var(--scale);
-    transition: var(--transition);
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:disabled {
-    opacity: 0.5;
-    transform: none;
-  }
-`;
+export const StyledButton = styled(Button).attrs({
+  variant: 'contained',
+  color: 'primary'
+})``;
