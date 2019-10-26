3bd481743faccfd55c7914334a91fad38e4c33eb
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-26 15:49:41 +0300

    Remove unused components

--- components/atoms/icons/ShortcutIcon.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { IconProps } from '../../../interfaces';
-import { Icon } from './Icon';
-
-export const ShortcutIcon: React.FC<IconProps> = props => <Icon size={3} {...props} />;
--- components/atoms/icons/index.ts
+++
@@ -1,3 +0,0 @@
-export { Icon } from './Icon';
-export { NavbarIcon } from './NavbarIcon';
-export { ShortcutIcon } from './ShortcutIcon';
