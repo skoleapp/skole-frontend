8b86326632fd5a3a38a0279c1c1ee9f2f4122bc3
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-26 13:58:41 +0300

    Update logout card

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,22 +1,11 @@
-import { Button, Typography } from '@material-ui/core';
 import { NextPage } from 'next';
-import Link from 'next/link';
 import React from 'react';
-import { Anchor, Card, MainLayout } from '../components';
+import { LogoutCard, MainLayout } from '../components';
 import { withPublic } from '../lib';
 
 const LogoutPage: NextPage = () => (
   <MainLayout title="Login">
-    <Card>
-      <Typography variant="h3">You have been logged out!</Typography>
-      <Link href="/login">
-        <Anchor>
-          <Button variant="contained" color="primary">
-            log back in
-          </Button>
-        </Anchor>
-      </Link>
-    </Card>
+    <LogoutCard />
   </MainLayout>
 );
 
