99ad6a31282a6ef3ab66e774674fccc9d481620b
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-26 16:58:02 +0300

    Final updates

--- components/atoms/form-fields/SelectFormField.tsx
+++ components/atoms/form-fields/SelectFormField.tsx
@@ -1,6 +1,7 @@
 import { FormControl, FormHelperText, InputLabel, MenuItem, Select } from '@material-ui/core';
 import { FieldProps, getIn } from 'formik';
 import React from 'react';
+import styled from 'styled-components';
 
 interface SelectFormFieldProps extends FieldProps {
   label?: string;
@@ -17,16 +18,22 @@ export const SelectFormField: React.FC<SelectFormFieldProps> = ({
   const errorText = getIn(form.touched, field.name) && getIn(form.errors, field.name);
 
   return (
-    <FormControl fullWidth error={!!errorText}>
-      {label && <InputLabel>{label}</InputLabel>}
-      <Select fullWidth {...field} {...props}>
-        {options.map((o, i) => (
-          <MenuItem key={i} value={o.value}>
-            {o.label}
-          </MenuItem>
-        ))}
-      </Select>
-      <FormHelperText>{errorText}</FormHelperText>
-    </FormControl>
+    <StyledSelectFormField>
+      <FormControl fullWidth error={!!errorText}>
+        {label && <InputLabel>{label}</InputLabel>}
+        <Select fullWidth {...field} {...props}>
+          {options.map((o, i) => (
+            <MenuItem key={i} value={o.value}>
+              {o.label}
+            </MenuItem>
+          ))}
+        </Select>
+        <FormHelperText>{errorText}</FormHelperText>
+      </FormControl>
+    </StyledSelectFormField>
   );
 };
+
+const StyledSelectFormField = styled.div`
+  margin-top: 1rem;
+`;
--- components/organisms/UserInfoCard.tsx
+++ components/organisms/UserInfoCard.tsx
@@ -1,34 +1,37 @@
-import { Button, Typography, Avatar } from '@material-ui/core';
-import Link from 'next/link';
+import { Avatar, Button, Typography } from '@material-ui/core';
+import Router from 'next/router';
 import React from 'react';
 import styled from 'styled-components';
 import { User } from '../../interfaces';
 import { Card } from '../atoms';
 
-const InfoSection = styled.div`
-  margin-top: 1rem;
-`;
-
 export const UserInfoCard: React.FC<User> = ({ id, title, username, bio, points }) => (
   <StyledCard>
     <Typography variant="h5">{title}</Typography>
     <Avatar src="https://myconstructor.gr/img/customers-imgs/avatar.png" />
-    <InfoSection>
+    <div className="info-section">
       <Typography variant="body1">{username}</Typography>
       <Typography variant="body1">Bio: {bio}</Typography>
       <Typography variant="body1">Points: {points}</Typography>
-    </InfoSection>
-    <Link href={`/user/${id}/edit`}>
-      <Button variant="contained" color="primary" fullWidth>
-        edit account
-      </Button>
-    </Link>
+    </div>
+    <Button
+      onClick={(): Promise<boolean> => Router.push(`/user/${id}/edit`)}
+      variant="contained"
+      color="primary"
+      fullWidth
+    >
+      edit account
+    </Button>
   </StyledCard>
 );
 
 const StyledCard = styled(Card)`
-  p {
-    margin: 0.5rem 0;
+  .info-section {
+    margin-top: 1rem;
+
+    p {
+      margin: 0.5rem 0;
+    }
   }
 
   .MuiAvatar-root {
