7dc82ba32197c2c7f8ab6ac489459e85683a80b1
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-26 13:10:57 +0300

    Remove unused components

--- components/atoms/headers/H2.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const H2 = styled.h2`
-  text-shadow: var(--text-shadow);
-`;
--- components/atoms/inputs/index.ts
+++
@@ -1,7 +0,0 @@
-export { CheckboxFormField } from './CheckboxFormField';
-export { Label } from './Label';
-export { SearchInput } from './SearchInput';
-export { SelectFormField } from './SelectFormField';
-export { Textarea } from './Textarea';
-export { TextInput } from './TextInput';
-export { TextInputFormField } from './TextInputFormField';
