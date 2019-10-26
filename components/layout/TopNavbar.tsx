e347c4277a7cd115d7bcb472bc5afc2d6e588bbf
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-26 13:11:58 +0300

    Update top navbar

---
+++ components/layout/TopNavbar.tsx
@@ -0,0 +1,41 @@
+import { AppBar, Toolbar } from '@material-ui/core';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { State } from '../../interfaces';
+import { SM } from '../../utils';
+import { Logo } from '../atoms';
+import { PublicAuthButtons } from '../molecules';
+import { AuthMenu, SearchWidget } from '../organisms';
+
+const StyledTopNavbar = styled.div`
+  flex-grow: 1;
+
+  .auth-menu,
+  .search,
+  .input-input,
+  .search-icon,
+  .public-auth-buttons {
+    display: none !important;
+
+    @media only screen and (min-width: ${SM}) {
+      display: flex !important;
+    }
+  }
+`;
+
+export const TopNavbar: React.FC = () => {
+  const { authenticated } = useSelector((state: State) => state.auth);
+
+  return (
+    <StyledTopNavbar>
+      <AppBar position="static">
+        <Toolbar>
+          <Logo />
+          <SearchWidget />
+          {authenticated ? <AuthMenu /> : <PublicAuthButtons />}
+        </Toolbar>
+      </AppBar>
+    </StyledTopNavbar>
+  );
+};
