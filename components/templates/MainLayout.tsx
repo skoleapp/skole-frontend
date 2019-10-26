f8590deebac6a13eb9f7bba5bbe97f79ae5662dd
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-26 15:50:35 +0300

    Update main layout

--- components/templates/MainLayout.tsx
+++ components/templates/MainLayout.tsx
@@ -1,39 +1,23 @@
 import React, { ReactNode } from 'react';
-import { useSelector } from 'react-redux';
-import styled from 'styled-components';
-import { State } from '../../interfaces';
 import { Container } from '../containers';
 import { BottomNavbar, Footer, Head, TopNavbar } from '../layout';
 
-interface StyledMainLayoutProps {
-  secondary?: boolean;
-  menuOpen: boolean;
-}
-
-const StyledMainLayout = styled.div<StyledMainLayoutProps>`
-  background: ${({ secondary }): string =>
-    (secondary && 'var(--secondary-bg)') || 'var(--primary-bg)'};
-  position: ${({ menuOpen }): string => (menuOpen ? 'fixed' : 'relative')};
-  min-height: 100%;
-  width: 100%;
-`;
+// const StyledMainLayout = styled.div`
+//   min-height: 100%;
+//   width: 100%;
+// `;
 
 interface Props {
   title?: string;
   children?: ReactNode;
-  secondary?: boolean;
 }
 
-export const MainLayout: React.FC<Props> = ({ title, children, secondary }) => {
-  const { menuOpen } = useSelector((state: State) => state.ui);
-
-  return (
-    <StyledMainLayout secondary={secondary} menuOpen={menuOpen}>
-      <Head title={title} />
-      <TopNavbar />
-      <Container>{children}</Container>
-      <BottomNavbar />
-      <Footer />
-    </StyledMainLayout>
-  );
-};
+export const MainLayout: React.FC<Props> = ({ title, children }) => (
+  <>
+    <Head title={title} />
+    <TopNavbar />
+    <Container>{children}</Container>
+    <BottomNavbar />
+    <Footer />
+  </>
+);
