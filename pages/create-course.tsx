23630a5cf97b9b9ed8d1259483d72ed3607ae87b
Wiljam Peltomaa <wiljam.peltomaa@congrid.fi>
2019-10-28 21:56:27 +0200

    Fix error

--- pages/create-course.tsx
+++ pages/create-course.tsx
@@ -1,12 +1,12 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
-import { Layout, Text } from '../components';
+import { Layout } from '../components';
 import { withAuth } from '../lib';
 
 const CreateCoursePage: React.FC = () => (
   <Layout title="Create Course">
     <Typography variant="h5">Create Course</Typography>
-    <Text>Course creation form will be shown here.</Text>
+    <Typography variant="body1">Course creation form will be shown here.</Typography>
   </Layout>
 );
 
