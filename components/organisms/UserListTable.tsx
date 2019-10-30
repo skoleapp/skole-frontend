8a32ed8c2a8257a4ae8271d73546d3b920be8a22
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-31 01:21:10 +0200

    Update user list table

--- components/organisms/UserListTable.tsx
+++ components/organisms/UserListTable.tsx
@@ -1,10 +1,61 @@
+import {
+  Avatar,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography
+} from '@material-ui/core';
+import Link from 'next/link';
 import React from 'react';
+import styled from 'styled-components';
 import { PublicUser } from '../../interfaces';
 
-// interface Props {
-//   users: PublicUser[];
-// }
+interface Props {
+  users: PublicUser[];
+}
 
-export const UserListTable: any = ({ users }: any) => {
-  return users.map((user: PublicUser, idx: number) => <div key={idx}>{user.username}</div>);
-};
+export const UserListTable: React.FC<Props> = ({ users }) => (
+  <StyledUserListTable>
+    <Paper>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Users</TableCell>
+            <TableCell align="right">Points</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user: PublicUser, i: number) => (
+            <Link href={`/user/${user.id}`} key={i}>
+              <TableRow>
+                <TableCell className="main-cell">
+                  <Avatar src="https://myconstructor.gr/img/customers-imgs/avatar.png" />
+                  <Typography variant="subtitle1">{user.username}</Typography>
+                </TableCell>
+                <TableCell>{user.points}</TableCell>
+              </TableRow>
+            </Link>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  </StyledUserListTable>
+);
+
+const StyledUserListTable = styled.div`
+  tr:hover {
+    background-color: var(--light-opacity);
+  }
+
+  .main-cell {
+    display: flex;
+    align-items: center;
+
+    h6 {
+      margin-left: 1rem;
+    }
+  }
+`;
