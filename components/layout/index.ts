53f205904048b57cbfd546987d7ef80ae597893d
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-30 19:33:17 +0200

    Replace loading indicator and remove loading screen

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,5 +1,4 @@
 export { BottomNavbar } from './BottomNavbar';
 export { Footer } from './Footer';
 export { HeadComponent as Head } from './Head';
-export { LoadingScreen } from './LoadingScreen';
 export { TopNavbar } from './TopNavbar';
