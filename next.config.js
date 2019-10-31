d3e3f29b6903c6b635e61bf8a89a996c8d3f4552
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-31 16:47:30 +0200

    Improve performance with magnitude of two

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
 const withCSS = require('@zeit/next-css');
-const withAssetsImport = require('next-assets-import');
 const WebpackBar = require('webpackbar');
 
-module.exports = withCSS(
-  withAssetsImport({
-    webpack: config => {
-      config.plugins.push(
-        new WebpackBar({
-          fancy: true,
-          profile: true,
-          basic: false
-        })
-      );
+module.exports = withCSS({
+  webpack: config => {
+    config.plugins.push(
+      new WebpackBar({
+        fancy: true,
+        profile: true,
+        basic: false
+      })
+    );
 
-      return config;
-    }
-  })
-);
+    return config;
+  }
+});
