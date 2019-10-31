3cba5897b043743a950a19c117b3d859770f4452
Wiljam Peltomaa <wiljam.peltomaa@congrid.fi>
2019-10-31 17:58:42 +0200

    Smh

--- components/form-fields/TextFormField.tsx
+++ components/form-fields/TextFormField.tsx
@@ -7,16 +7,12 @@ export const TextFormField: React.FC<FieldProps> = ({ field, form, ...props }) =
   const errorText = getIn(form.touched, field.name) && getIn(form.errors, field.name);
 
   return (
-    <StyledTextFormField
-      fullWidth
-      helperText={errorText}
-      error={!!errorText}
-      {...field}
-      {...props}
-    />
+    <StyledTextFormField>
+      <TextField fullWidth helperText={errorText} error={!!errorText} {...field} {...props} />
+    </StyledTextFormField>
   );
 };
 
-const StyledTextFormField = styled(TextField)`
+const StyledTextFormField = styled.div`
   margin: 0.5rem 0 !important;
 `;
