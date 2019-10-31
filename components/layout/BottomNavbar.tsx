78cbc6a37eb658a42377828e26e8c25eaf292ff3
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-31 12:18:20 +0200

    Update bottom navbar

--- components/layout/BottomNavbar.tsx
+++ components/layout/BottomNavbar.tsx
@@ -1,51 +1,33 @@
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
-import { AccountCircle, Backup, PermIdentity } from '@material-ui/icons';
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import { AccountCircle, Home } from '@material-ui/icons';
 import SearchIcon from '@material-ui/icons/Search';
 import Router from 'next/router';
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { ChangeEvent, useState } from 'react';
 import styled from 'styled-components';
-import { State } from '../../interfaces';
 import { breakpoints } from '../../styles';
 
 export const BottomNavbar: React.FC = () => {
   const [value, setValue] = useState(0);
-  const { authenticated } = useSelector((state: State) => state.auth);
+
+  const handleChange = (_e: ChangeEvent<HTMLButtonElement>, newValue: number): void => {
+    setValue(newValue);
+  };
 
   return (
     <StyledBottomNavbar>
-      <BottomNavigation
-        value={value}
-        onChange={(_e, newValue): void => setValue(newValue)}
-        showLabels
-      >
+      <BottomNavigation value={value} onChange={handleChange} showLabels>
         <BottomNavigationAction
           onClick={(): Promise<boolean> => Router.push('/search')}
           icon={<SearchIcon />}
         />
         <BottomNavigationAction
-          onClick={(): Promise<boolean> => Router.push('/upload-resource')}
-          icon={<Backup />}
+          onClick={(): Promise<boolean> => Router.push('/')}
+          icon={<Home />}
         />
-        {authenticated && (
-          <BottomNavigationAction
-            onClick={(): Promise<boolean> => Router.push('/account/courses')}
-            icon={<FavoriteIcon />}
-          />
-        )}
-        {authenticated && (
-          <BottomNavigationAction
-            onClick={(): Promise<boolean> => Router.push('/account')}
-            icon={<AccountCircle />}
-          />
-        )}
-        {!authenticated && (
-          <BottomNavigationAction
-            onClick={(): Promise<boolean> => Router.push('/login')}
-            icon={<PermIdentity />}
-          />
-        )}
+        <BottomNavigationAction
+          onClick={(): Promise<boolean> => Router.push('/account')}
+          icon={<AccountCircle />}
+        />{' '}
       </BottomNavigation>
     </StyledBottomNavbar>
   );
