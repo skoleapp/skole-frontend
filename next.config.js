fe1ca8f0a51c1b2b141b0f3949bc352b0e1cbf30
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-03 16:58:24 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@ const withCSS = require('@zeit/next-css');
 const WebpackBar = require('webpackbar');
 
 module.exports = withCSS({
+  target: 'serverless',
+  env: {
+    API_URL: process.env.API_URL
+  },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    };
+
+    return config;
+  },
   webpack: config => {
     config.plugins.push(
       new WebpackBar({
