8d9db9ba5e185a25894b021147bbe6c2d5065bb3
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-05 00:40:36 +0200

    Update form error message

--- components/containers/FormErrorMessage.tsx
+++ components/containers/FormErrorMessage.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const FormErrorMessage = styled.div`
   color: var(--danger) !important;
   margin: 0.5rem 0;
-  font-size: 0.75rem;
+  font-size: 0.85rem;
   text-align: left;
 `;
