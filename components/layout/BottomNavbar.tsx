311b7790b72f19ac28dcc68c5e783afca7b21471
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-05 01:20:33 +0200

    Update bottom navbar

--- components/layout/BottomNavbar.tsx
+++ components/layout/BottomNavbar.tsx
@@ -1,13 +1,35 @@
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import { AccountCircle, Home } from '@material-ui/icons';
 import SearchIcon from '@material-ui/icons/Search';
-import Router from 'next/router';
-import React, { ChangeEvent, useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { breakpoints } from '../../styles';
 
 export const BottomNavbar: React.FC = () => {
   const [value, setValue] = useState(0);
+  const router = useRouter();
+
+  const getValue = () => {
+    switch (router.pathname) {
+      case '/search': {
+        return 0;
+      }
+      case '/': {
+        return 1;
+      }
+      case '/account': {
+        return 2;
+      }
+      default: {
+        return 0;
+      }
+    }
+  };
+
+  useEffect(() => {
+    setValue(getValue());
+  }, []);
 
   const handleChange = (_e: ChangeEvent<HTMLButtonElement>, newValue: number): void => {
     setValue(newValue);
@@ -17,15 +39,15 @@ export const BottomNavbar: React.FC = () => {
     <StyledBottomNavbar>
       <BottomNavigation value={value} onChange={handleChange} showLabels>
         <BottomNavigationAction
-          onClick={(): Promise<boolean> => Router.push('/search')}
+          onClick={(): Promise<boolean> => router.push('/search')}
           icon={<SearchIcon />}
         />
         <BottomNavigationAction
-          onClick={(): Promise<boolean> => Router.push('/')}
+          onClick={(): Promise<boolean> => router.push('/')}
           icon={<Home />}
         />
         <BottomNavigationAction
-          onClick={(): Promise<boolean> => Router.push('/account')}
+          onClick={(): Promise<boolean> => router.push('/account')}
           icon={<AccountCircle />}
         />{' '}
       </BottomNavigation>
