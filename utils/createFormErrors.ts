1dae3b0cdfe0beb91e84efccaa4ba1b3debb39f7
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-05 21:31:11 +0200

    Update form errors

--- utils/createFormErrors.ts
+++ utils/createFormErrors.ts
@@ -1,5 +1,3 @@
-import { FormErrors } from '../interfaces';
-
 const snakeToCamel = (str: string): string =>
   str.replace(/([-_][a-z])/g, group =>
     group
@@ -8,31 +6,9 @@ const snakeToCamel = (str: string): string =>
       .replace('_', '')
   );
 
-const authForms = {
-  username: '',
-  email: '',
-  password: '',
-  confirmPassword: ''
-};
-
-const updateUserForm = {
-  title: '',
-  bio: '',
-  language: ''
-};
-
-const changePasswordForm = {
-  oldPassword: '',
-  newPassword: '',
-  confirmNewPassword: ''
-};
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const createFormErrors = (errors: any): FormErrors => {
-  const formErrors = {
-    ...authForms,
-    ...updateUserForm,
-    ...changePasswordForm,
+export const createFormErrors = (errors: any): any => {
+  let formErrors = {
     general: ''
   };
 
@@ -43,18 +19,13 @@ export const createFormErrors = (errors: any): FormErrors => {
   if (errors.length) {
     // eslint-disable-next-line
     errors.map((e: any) => {
-      if (e.field) {
+      if (e.field === '__all__') {
+        formErrors.general = e.messages.join();
+      } else if (e.field) {
         (formErrors as any)[snakeToCamel(e.field)] = e.messages.join(); // eslint-disable-line @typescript-eslint/no-explicit-any
       } else {
         formErrors.general = e.messages.join();
       }
-    });
-  }
-
-  if (errors.graphQLErrors) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    errors.graphQLErrors.map((e: any) => {
-      formErrors.general = e.message;
     });
   }
 
