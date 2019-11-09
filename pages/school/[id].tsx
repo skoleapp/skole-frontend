11bb08f7009c3051a1cb22d701766f659dd6307d
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-09 10:38:39 +0200

    Update school detail page

--- pages/school/[id].tsx
+++ pages/school/[id].tsx
@@ -1,13 +1,54 @@
 import { Typography } from '@material-ui/core';
 import { NextPage } from 'next';
 import React from 'react';
-import { Layout } from '../../containers';
-import { withAuthSync } from '../../utils';
+import { compose } from 'redux';
+import { StyledCard } from '../../components';
+import { Layout, NotFoundCard } from '../../containers';
+import { SchoolDocument } from '../../generated/graphql';
+import { School, SkoleContext } from '../../interfaces';
+import { withApollo, withRedux } from '../../lib';
+import { useSSRAuthSync } from '../../utils';
 
-const SchoolDetailPage: NextPage = () => (
-  <Layout title="School Detail">
-    <Typography variant="h5">School Detail</Typography>
-  </Layout>
-);
+interface Props {
+  school: School | null;
+}
 
-export default withAuthSync(SchoolDetailPage);
+const SchoolDetailPage: NextPage<Props> = ({ school }) => {
+  if (school) {
+    return (
+      <Layout title={school.name}>
+        <StyledCard>
+          <Typography variant="h5">{school.name}</Typography>
+          <Typography variant="body1">Type: {school.schoolType}</Typography>
+          <Typography variant="body1">Name: {school.name}</Typography>
+          <Typography variant="body1">City: {school.city}</Typography>
+          <Typography variant="body1">Country: {school.country}</Typography>
+        </StyledCard>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout title="School not found">
+      <NotFoundCard text="School not found." />
+    </Layout>
+  );
+};
+
+SchoolDetailPage.getInitialProps = async (ctx: SkoleContext): Promise<Props> => {
+  await useSSRAuthSync(ctx);
+  const { apolloClient, query } = ctx;
+
+  try {
+    const { id } = query;
+    const { data } = await apolloClient.query({ query: SchoolDocument, variables: { id } });
+    return { school: data.school };
+  } catch (error) {
+    return { school: null };
+  }
+};
+
+export default compose(
+  withRedux,
+  withApollo
+)(SchoolDetailPage as NextPage);
