c3b33a0cf72b0e68b08934268ede9dbcc42be0ca
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-11 22:07:25 +0200

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,19 @@
-import { Link, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
+import { TextLink } from './TextLink';
 
 export const Footer: React.FC = () => (
   <StyledFooter>
-    <Typography variant="h6">© {new Date().getFullYear()} Skole Ltd.</Typography>
-    <Link href="/feedback" color="secondary">
+    <Typography variant="subtitle1">© {new Date().getFullYear()} Skole Ltd.</Typography>
+    <TextLink href="/feedback" color="secondary">
       Feedback
-    </Link>
+    </TextLink>
   </StyledFooter>
 );
 
 const StyledFooter = styled.div`
-  height: 8rem;
+  height: 5rem;
   background: var(--primary);
   color: var(--white);
   display: flex;
