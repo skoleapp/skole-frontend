a62c3694020f9cf114d4626fe5c0440edd74ec8a
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-17 14:44:42 +0200

    Update create course form

--- containers/CreateCourseForm.tsx
+++ containers/CreateCourseForm.tsx
@@ -1,8 +1,7 @@
-import { FormControl, InputLabel, MenuItem } from '@material-ui/core';
-import { ErrorMessage, Field, FormikProps } from 'formik';
-import { Select, TextField } from 'formik-material-ui';
+import { Field, FormikProps } from 'formik';
+import { TextField } from 'formik-material-ui';
 import React from 'react';
-import { FormErrorMessage, FormSubmitSection, StyledForm } from '../components';
+import { FormSubmitSection, SchoolField, StyledForm, SubjectField } from '../components';
 import { CreateCourseFormValues, School, Subject } from '../interfaces';
 
 interface Props extends CreateCourseFormValues {
@@ -26,34 +25,8 @@ export const CreateCourseForm: React.FC<FormikProps<Props>> = props => (
       component={TextField}
       fullWidth
     />
-    <FormControl fullWidth>
-      <InputLabel>Subject</InputLabel>
-      <Field name="subject" component={Select} fullWidth>
-        {props.values.subjects &&
-          props.values.subjects.map(
-            (s: Subject, i: number): JSX.Element => (
-              <MenuItem key={i} value={s.id}>
-                {s.name}
-              </MenuItem>
-            )
-          )}
-      </Field>
-      <ErrorMessage name="subject" component={FormErrorMessage} />
-    </FormControl>
-    <FormControl fullWidth>
-      <InputLabel>School</InputLabel>
-      <Field name="school" component={Select} fullWidth>
-        {props.values.schools &&
-          props.values.schools.map(
-            (s: School, i: number): JSX.Element => (
-              <MenuItem key={i} value={s.id}>
-                {s.name}
-              </MenuItem>
-            )
-          )}
-      </Field>
-      <ErrorMessage name="school" component={FormErrorMessage} />
-    </FormControl>
+    <SubjectField {...props} />
+    <SchoolField {...props} />
     <FormSubmitSection submitButtonText="save" {...props} />
   </StyledForm>
 );
