567406aab393163ca34a80be8753858f648526bd
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-17 16:03:47 +0200

    Update subjects page

--- pages/subjects.tsx
+++ pages/subjects.tsx
@@ -1,22 +1,44 @@
 import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@material-ui/core';
+import { Formik, FormikActions } from 'formik';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import * as R from 'ramda';
 import React from 'react';
 import { compose } from 'redux';
 import { StyledTable } from '../components';
-import { Layout } from '../containers';
-import { SkoleContext, Subject } from '../interfaces';
+import { FilterSubjectsForm, Layout } from '../containers';
+import { SchoolsAndSubjectsDocument } from '../generated/graphql';
+import { FilterSubjectsFormValues, School, SkoleContext, Subject } from '../interfaces';
 import { withApollo, withRedux } from '../lib';
 import { useSSRAuthSync } from '../utils';
 
 interface Props {
+  schools?: School[];
   subjects?: Subject[];
 }
 
-const SubjectsPage: NextPage<Props> = ({ subjects }) => {
+const SubjectsPage: NextPage<Props> = ({ schools, subjects }) => {
   const router = useRouter();
-  const query = router;
+  const { query, pathname } = router;
+  const { schoolId } = query;
+
+  // Pick non-empty values and reload the page with new query params.
+  const handleSubmit = (
+    values: FilterSubjectsFormValues,
+    actions: FormikActions<FilterSubjectsFormValues>
+  ) => {
+    const isNotEmpty = (val: string) => val !== '';
+    const query: any = R.pickBy(isNotEmpty, values); // eslint-disable-line @eslint/typescript-no-explicit-any
+    router.push({ pathname, query });
+    actions.setSubmitting(false);
+  };
+
+  // Pre-load query params to the form.
+  const initialValues = {
+    schoolId: schoolId || '',
+    schools: schools || []
+  };
 
   return (
     <Layout heading="Subjects" title="Subjects" backUrl="/">
@@ -24,8 +46,13 @@ const SubjectsPage: NextPage<Props> = ({ subjects }) => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>
-                <Typography variant="h6">Subjects</Typography>
+              <TableCell align="center">
+                <Typography variant="h6">Filter Subjects</Typography>
+                <Formik
+                  component={FilterSubjectsForm}
+                  onSubmit={handleSubmit}
+                  initialValues={initialValues}
+                />
               </TableCell>
             </TableRow>
           </TableHead>
@@ -62,17 +89,16 @@ const SubjectsPage: NextPage<Props> = ({ subjects }) => {
 
 SubjectsPage.getInitialProps = async (ctx: SkoleContext): Promise<Props> => {
   await useSSRAuthSync(ctx);
-  // const { apolloClient, query } = ctx;
+  const { apolloClient, query } = ctx;
 
   try {
-    // const { id } = query;
-    // const { data } = await apolloClient.query({
-    //   query: SchoolSubjectsDocument,
-    //   variables: { id }
-    // });
+    const { data } = await apolloClient.query({
+      query: SchoolsAndSubjectsDocument,
+      variables: { ...query }
+    });
 
-    // return { school: data.school };
-    return { subjects: [] };
+    const { schools, subjects } = data;
+    return { schools, subjects };
   } catch (error) {
     return {};
   }
