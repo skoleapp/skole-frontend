6047ab0d6ff3150f5e6f8ae791bca95410951dfb
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-17 16:01:01 +0200

    Update courses page

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,40 +1,63 @@
-import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@material-ui/core';
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography
+} from '@material-ui/core';
+import { Formik, FormikActions } from 'formik';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import React from 'react';
+import * as R from 'ramda';
+import React, { useRef } from 'react';
 import { compose } from 'redux';
 import { LabelTag, StyledTable } from '../../components';
-import { Layout, SubjectFilter } from '../../containers';
-import { CoursesDocument } from '../../generated/graphql';
-import { Course, School, SkoleContext, Subject } from '../../interfaces';
+import { FilterCoursesForm, Layout } from '../../containers';
+import { CoursesSchoolsAndSubjectsDocument } from '../../generated/graphql';
+import { Course, FilterCoursesFormValues, School, SkoleContext, Subject } from '../../interfaces';
 import { withApollo, withRedux } from '../../lib';
 import { useSSRAuthSync } from '../../utils';
 
 interface Props {
   courses?: Course[];
-  school?: School;
+  schools?: School[];
   subjects?: Subject[];
 }
 
-const CoursesPage: NextPage<Props> = ({ courses, school, subjects }) => {
+const CoursesPage: NextPage<Props> = ({ courses, schools, subjects }) => {
   const router = useRouter();
-  const { subjectId } = router.query;
-  const subject = subjects && subjects.find((s: Subject) => s.id.toString() === subjectId);
+  const { query, pathname } = router;
+  const { courseName, courseCode, subjectId, schoolId } = query;
+  const ref = useRef<any>(); // eslint-disable-line @eslint/typescript-no-explicit-any
 
-  const getTitle = () => {
-    if (school) {
-      if (subject) {
-        return `${subject.name} Courses in ${school.name}`;
-      } else {
-        return `All courses in ${school.name}`;
-      }
-    } else {
-      if (subject) {
-        return `All ${subject.name} Courses`;
-      } else {
-        return 'All courses';
-      }
-    }
+  // Pick non-empty values and reload the page with new query params.
+  const handleSubmit = (
+    values: FilterCoursesFormValues,
+    actions: FormikActions<FilterCoursesFormValues>
+  ) => {
+    const isNotEmpty = (val: string, key: string) =>
+      key !== 'subjects' && key !== 'schools' && val !== '';
+    const query: any = R.pickBy(isNotEmpty, values); // eslint-disable-line @eslint/typescript-no-explicit-any
+    router.push({ pathname, query });
+    actions.setSubmitting(false);
+  };
+
+  // Wait for the router to clear the query params, then reset the form.
+  const handleClearFilters = async () => {
+    await router.push(pathname);
+    ref.current.resetForm();
+  };
+
+  // Pre-load query params to the form.
+  const initialValues = {
+    courseName: courseName || '',
+    courseCode: courseCode || '',
+    subjectId: subjectId || '',
+    schoolId: schoolId || '',
+    subjects: subjects || [],
+    schools: schools || []
   };
 
   return (
@@ -43,16 +66,24 @@ const CoursesPage: NextPage<Props> = ({ courses, school, subjects }) => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>
-                <Typography variant="h6">{getTitle()}</Typography>
-                <SubjectFilter subjects={subjects} initialValue={subjectId} />
+              <TableCell align="center">
+                <Typography variant="h6">Filter Courses</Typography>
+                <Formik
+                  component={FilterCoursesForm}
+                  onSubmit={handleSubmit}
+                  initialValues={initialValues}
+                  ref={ref}
+                />
+                <Button variant="outlined" color="primary" fullWidth onClick={handleClearFilters}>
+                  clear filters
+                </Button>
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {courses && courses.length ? (
               courses.map((course: Course, i: number) => (
-                <TableRow key={i} onClick={() => router.push(`/course/${course.id}`)}>
+                <TableRow key={i} onClick={() => router.push(`/courses/${course.id}`)}>
                   <TableCell>
                     <Typography variant="subtitle1">{course.name}</Typography>
                     {course.code && <LabelTag text={course.code} />}
@@ -78,16 +109,13 @@ CoursesPage.getInitialProps = async (ctx: SkoleContext): Promise<Props> => {
   const { apolloClient, query } = ctx;
 
   try {
-    const { schoolId, subjectId } = query;
-
     const { data } = await apolloClient.query({
-      query: CoursesDocument,
-      variables: { schoolId, subjectId }
+      query: CoursesSchoolsAndSubjectsDocument,
+      variables: { ...query }
     });
 
-    const { courses, school, subjects } = data;
-    return { courses, school, subjects };
-  } catch (err) {
+    return { ...data };
+  } catch {
     return {};
   }
 };
