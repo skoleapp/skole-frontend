aae0c8c0c1a8b966e1f8576a1c174ae88b548f56
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-17 14:42:58 +0200

    Add common fields for filter forms

---
+++ components/SchoolField.tsx
@@ -0,0 +1,24 @@
+import { FormControl, InputLabel, MenuItem } from '@material-ui/core';
+import { ErrorMessage, Field, FormikProps } from 'formik';
+import { Select } from 'formik-material-ui';
+import React from 'react';
+import { School } from '../interfaces';
+import { FormErrorMessage } from './FormErrorMessage';
+
+export const SchoolField: React.FC<FormikProps<any>> = props => (
+  <FormControl fullWidth>
+    <InputLabel>School</InputLabel>
+    <Field name="schoolId" component={Select} placeholder="test" fullWidth>
+      <MenuItem value="">---</MenuItem>
+      {props.values.schools &&
+        props.values.schools.map(
+          (s: School, i: number): JSX.Element => (
+            <MenuItem key={i} value={s.id}>
+              {s.name}
+            </MenuItem>
+          )
+        )}
+    </Field>
+    <ErrorMessage name="schoolId" component={FormErrorMessage} />
+  </FormControl>
+);
---
+++ components/SubjectField.tsx
@@ -0,0 +1,24 @@
+import { FormControl, InputLabel, MenuItem } from '@material-ui/core';
+import { ErrorMessage, Field, FormikProps } from 'formik';
+import { Select } from 'formik-material-ui';
+import React from 'react';
+import { Subject } from '../interfaces';
+import { FormErrorMessage } from './FormErrorMessage';
+
+export const SubjectField: React.FC<FormikProps<any>> = props => (
+  <FormControl fullWidth>
+    <InputLabel>Subject</InputLabel>
+    <Field name="subjectId" component={Select} fullWidth>
+      <MenuItem value="">---</MenuItem>
+      {props.values.subjects &&
+        props.values.subjects.map(
+          (s: Subject, i: number): JSX.Element => (
+            <MenuItem key={i} value={s.id}>
+              {s.name}
+            </MenuItem>
+          )
+        )}
+    </Field>
+    <ErrorMessage name="subjectId" component={FormErrorMessage} />
+  </FormControl>
+);
