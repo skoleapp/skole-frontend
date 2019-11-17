66e10425f1750686a4ba7d3e825be9db412f3809
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-17 16:02:21 +0200

    Update schools page

--- pages/schools/index.tsx
+++ pages/schools/index.tsx
@@ -1,35 +1,94 @@
-import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@material-ui/core';
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography
+} from '@material-ui/core';
+import { Formik, FormikActions } from 'formik';
 import { NextPage } from 'next';
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/router';
+import * as R from 'ramda';
+import React, { useRef } from 'react';
 import { compose } from 'redux';
+import * as Yup from 'yup';
 import { LabelTag, StyledTable } from '../../components';
-import { Layout, NotFoundCard } from '../../containers';
+import { FilterSchoolsForm, Layout } from '../../containers';
 import { SchoolsDocument } from '../../generated/graphql';
-import { School, SkoleContext } from '../../interfaces';
+import { FilterSchoolsFormValues, School, SkoleContext } from '../../interfaces';
 import { withApollo, withRedux } from '../../lib';
 import { useSSRAuthSync } from '../../utils';
 
+const validationSchema = Yup.object().shape({
+  schoolType: Yup.string(),
+  city: Yup.string(),
+  country: Yup.string(),
+  name: Yup.string()
+});
+
 interface Props {
-  schools: School[] | null;
+  schools?: School[];
 }
 
 const SchoolsPage: NextPage<Props> = ({ schools }) => {
-  if (schools && schools.length) {
-    return (
-      <Layout title="Schools" backUrl="/">
-        <StyledTable>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Typography variant="h6">Schools</Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {schools.map((school: School, i: number) => (
-                <Link href={{ pathname: `schools/${school.id}` }} key={i}>
+  const router = useRouter();
+  const { query, pathname } = router;
+  const ref = useRef<any>(); // eslint-disable-line @eslint/typescript-no-explicit-any
+  const { schoolType, schoolCity, schoolCountry, schoolName } = query;
+
+  // Pick non-empty values and reload the page with new query params.
+  const handleSubmit = (
+    values: FilterSchoolsFormValues,
+    actions: FormikActions<FilterSchoolsFormValues>
+  ) => {
+    const isNotEmpty = (val: string) => val !== '';
+    const query: any = R.pickBy(isNotEmpty, values); // eslint-disable-line @eslint/typescript-no-explicit-any
+    router.push({ pathname, query });
+    actions.setSubmitting(false);
+  };
+
+  // Wait for the router to clear the query params, then reset the form.
+  const handleClearFilters = async () => {
+    await router.push(pathname);
+    ref.current.resetForm();
+  };
+
+  // Pre-load query params to the form.
+  const initialValues = {
+    schoolType: schoolType || '',
+    schoolName: schoolName || '',
+    schoolCity: schoolCity || '',
+    schoolCountry: schoolCountry || ''
+  };
+
+  return (
+    <Layout heading="Schools" title="Schools" backUrl="/">
+      <StyledTable>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">
+                <Typography variant="h6">Filter Schools</Typography>
+                <Formik
+                  component={FilterSchoolsForm}
+                  onSubmit={handleSubmit}
+                  validationSchema={validationSchema}
+                  initialValues={initialValues}
+                  ref={ref}
+                />
+                <Button variant="outlined" color="primary" fullWidth onClick={handleClearFilters}>
+                  clear filters
+                </Button>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {schools && schools.length ? (
+              schools.map((school: School, i: number) => (
+                <Link href={`/schools/${school.id}`} key={i}>
                   <TableRow>
                     <TableCell>
                       <Typography variant="subtitle1">{school.name}</Typography>
@@ -37,29 +96,30 @@ const SchoolsPage: NextPage<Props> = ({ schools }) => {
                     </TableCell>
                   </TableRow>
                 </Link>
-              ))}
-            </TableBody>
-          </Table>
-        </StyledTable>
-      </Layout>
-    );
-  } else {
-    return (
-      <Layout title="No schools found">
-        <NotFoundCard text="No schools found..." />
-      </Layout>
-    );
-  }
+              ))
+            ) : (
+              <TableRow>
+                <TableCell>
+                  <Typography variant="subtitle1">No schools...</Typography>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </StyledTable>
+    </Layout>
+  );
 };
 
 SchoolsPage.getInitialProps = async (ctx: SkoleContext): Promise<Props> => {
   await useSSRAuthSync(ctx);
+  const { query, apolloClient } = ctx;
 
   try {
-    const { data } = await ctx.apolloClient.query({ query: SchoolsDocument });
-    return { schools: data.schools };
-  } catch (error) {
-    return { schools: null };
+    const { data } = await apolloClient.query({ query: SchoolsDocument, variables: { ...query } });
+    return { ...data };
+  } catch {
+    return {};
   }
 };
 
