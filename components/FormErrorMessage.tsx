4f26adac0ab92b978f5e0f1d3d26146ba5ce3ba9
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-17 16:56:18 +0200

    Update form error message

--- components/FormErrorMessage.tsx
+++ components/FormErrorMessage.tsx
@@ -1,6 +1,7 @@
+import { Box } from '@material-ui/core';
 import styled from 'styled-components';
 
-export const FormErrorMessage = styled.div`
+export const FormErrorMessage = styled(Box)`
   color: var(--danger) !important;
   margin: 0.5rem 0;
   font-size: 0.85rem;
