3e94088f3f3040a5ec3156e83a7c57fcb23be3ae
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-17 14:47:24 +0200

    Update store

--- lib/store.ts
+++ lib/store.ts
@@ -1,12 +1,7 @@
 import { applyMiddleware, createStore, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { State } from '../interfaces';
-import { initialAuthState, rootReducer } from '../reducers';
+import { rootReducer } from '../reducers';
 
-const initialState: State = {
-  auth: initialAuthState
-};
-
-export const initStore = (preloadedState = initialState): Store =>
+export const initStore = (preloadedState = {}): Store =>
   createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)));
