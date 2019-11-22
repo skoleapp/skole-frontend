3ab8df3f65984f9ddfedac334552fc4f7039be9a
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-22 17:01:21 +0200

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,29 @@
 import { Box, Typography } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
-import { breakpoints } from '../styles';
 import { TextLink } from './TextLink';
 
 export const Footer: React.FC = () => (
-  <StyledFooter display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-    <Typography variant="subtitle1">© {new Date().getFullYear()} Skole Ltd.</Typography>
-    <TextLink href="/feedback" color="secondary">
-      Feedback
-    </TextLink>
+  <StyledFooter
+    className="desktop-only"
+    display="flex"
+    flexDirection="column"
+    alignItems="center"
+    justifyContent="center"
+  >
+    <Typography variant="subtitle1" color="secondary">
+      © {new Date().getFullYear()} Skole Ltd.
+    </Typography>
     <TextLink href="/about" color="secondary">
       About
+    </TextLink>
+    <TextLink href="/contact" color="secondary">
+      Contact
     </TextLink>
   </StyledFooter>
 );
 
 const StyledFooter = styled(Box)`
   background: var(--primary);
-  color: var(--white);
   padding: 0.5rem 0;
-  height: 8rem;
-
-  @media only screen and (max-width: ${breakpoints.SM}) {
-    display: none !important;
-  }
 `;
