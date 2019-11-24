2f82e980a2b88db2fd13007566ef6f3e4097be32
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-24 19:24:12 +0200

    Update heading

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,37 +1,17 @@
-import { Box, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
-import { breakpoints } from '../styles';
 
 interface Props {
   text: string;
 }
 
 export const Heading: React.FC<Props> = ({ text }) => (
-  <StyledLogo>
-    <Typography variant="h6">{text}</Typography>
-  </StyledLogo>
+  <StyledHeading variant="h6">{text}</StyledHeading>
 );
 
-const StyledLogo = styled(Box)`
-  margin: 0 0.5rem;
-  flex-grow: 1;
-  text-align: center;
-
-  .MuiTypography-h6 {
-    max-width: 12rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    margin: 0 auto;
-
-    @media only screen and (min-width: ${breakpoints.SM}) {
-      max-width: 30rem;
-      margin-left: 0;
-    }
-  }
-
-  @media only screen and (min-width: ${breakpoints.SM}) {
-    text-align: left;
-  }
+const StyledHeading = styled(Typography)`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
