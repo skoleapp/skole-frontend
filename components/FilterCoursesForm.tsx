7d5d8260a51665388a83a2dfda86b67d75ee2df1
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-24 19:17:39 +0200

    Update filter courses form

---
+++ components/FilterCoursesForm.tsx
@@ -0,0 +1,32 @@
+import { Field, FormikProps } from 'formik';
+import { TextField } from 'formik-material-ui';
+import React from 'react';
+import { FormSubmitSection, SchoolField, StyledForm, SubjectField } from '.';
+import { FilterCoursesFormValues, School, Subject } from '../interfaces';
+
+interface Props extends FormikProps<FilterCoursesFormValues> {
+  subjects: Subject[];
+  schools: School[];
+}
+
+export const FilterCoursesForm: React.FC<Props> = props => (
+  <StyledForm>
+    <Field
+      name="courseName"
+      component={TextField}
+      label="Course Name"
+      placeholder="Course Name"
+      fullWidth
+    />
+    <Field
+      name="courseCode"
+      component={TextField}
+      label="Course Code"
+      placeholder="Course Code"
+      fullWidth
+    />
+    <SchoolField {...props} />
+    <SubjectField {...props} />
+    <FormSubmitSection submitButtonText="apply filters" {...props} />
+  </StyledForm>
+);
