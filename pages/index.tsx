5e6ff16b5073abfa8879117979ffb8007fe850a5
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-26 17:29:11 +0200

    Update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ import {
 import { NextPage } from 'next';
 import React from 'react';
 import { compose } from 'redux';
-import styled from 'styled-components';
 import { ButtonLink, Layout, Shortcut } from '../components';
 import { SkoleContext } from '../interfaces';
 import { withApollo, withRedux } from '../lib';
@@ -22,16 +21,20 @@ const IndexPage: NextPage = () => (
       <Typography variant="h5">Learn with Skole community!</Typography>
     </Box>
     <Grid container>
-      <ShortcutsRow item xs={12}>
-        <Shortcut text="Courses" icon={SchoolOutlined} href="/courses" />
-        <Shortcut text="Schools" icon={HouseOutlined} href="/schools" />
-        <Shortcut text="Subjects" icon={SubjectOutlined} href="/subjects" />
-      </ShortcutsRow>
-      <ShortcutsRow item xs={12}>
-        <Shortcut text="Upload Resource" icon={CloudUploadOutlined} href="/upload-resource" />
-        <Shortcut text="Create Course" icon={LibraryAddOutlined} href="/create-course" />
-        <Shortcut text="Leaderboard" icon={ScoreOutlined} href="/leaderboard" />
-      </ShortcutsRow>
+      <Grid item xs={12}>
+        <Box className="flex-flow" display="flex" justifyContent="center">
+          <Shortcut text="Courses" icon={SchoolOutlined} href="/courses" />
+          <Shortcut text="Schools" icon={HouseOutlined} href="/schools" />
+          <Shortcut text="Subjects" icon={SubjectOutlined} href="/subjects" />
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <Box className="flex-flow" display="flex" justifyContent="center">
+          <Shortcut text="Upload Resource" icon={CloudUploadOutlined} href="/upload-resource" />
+          <Shortcut text="Create Course" icon={LibraryAddOutlined} href="/create-course" />
+          <Shortcut text="Leaderboard" icon={ScoreOutlined} href="/leaderboard" />
+        </Box>
+      </Grid>
     </Grid>
     <Box marginY="1rem">
       <Typography variant="h6" gutterBottom>
@@ -43,12 +46,6 @@ const IndexPage: NextPage = () => (
     </Box>
   </Layout>
 );
-
-const ShortcutsRow = styled(Grid)`
-  display: flex;
-  justify-content: center;
-  flex-flow: row wrap;
-`;
 
 IndexPage.getInitialProps = async (ctx: SkoleContext): Promise<{}> => {
   await useAuthSync(ctx);
