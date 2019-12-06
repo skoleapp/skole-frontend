bcba5432bb5f20f5551d1a77fe33ebd5ee46510f
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-06 02:33:28 +0200

    Update shared components

---
+++ components/shared/ButtonLink.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-ui/core';
+import { ButtonProps } from '@material-ui/core/Button';
+import Link, { LinkProps } from 'next/link';
+import React from 'react';
+
+type Props = LinkProps & Omit<ButtonProps, 'href'>;
+
+export const ButtonLink: React.FC<Props> = ({ href, children, ...props }) => (
+  <Link href={href}>
+    <Button {...props}>{children}</Button>
+  </Link>
+);
