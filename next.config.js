3f8893d51e60739c944ffa4e28ebe13e893c3900
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-07 17:32:11 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,33 @@
 const withCSS = require('@zeit/next-css');
 const WebpackBar = require('webpackbar');
+const withAssetsImport = require('next-assets-import');
 
-module.exports = withCSS({
-  target: 'serverless',
-  env: {
-    API_URL: process.env.API_URL,
-    STATIC_URL: process.env.STATIC_URL,
-    MEDIA_URL: process.env.MEDIA_URL
-  },
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300
-    };
+module.exports = withAssetsImport(
+  withCSS({
+    target: 'serverless',
+    env: {
+      API_URL: process.env.API_URL,
+      STATIC_URL: process.env.STATIC_URL,
+      MEDIA_URL: process.env.MEDIA_URL
+    },
+    webpackDevMiddleware: config => {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300
+      };
 
-    return config;
-  },
-  webpack: config => {
-    config.plugins.push(
-      new WebpackBar({
-        fancy: true,
-        profile: true,
-        basic: false
-      })
-    );
+      return config;
+    },
+    webpack: config => {
+      config.plugins.push(
+        new WebpackBar({
+          fancy: true,
+          profile: true,
+          basic: false
+        })
+      );
 
-    return config;
-  }
-});
+      return config;
+    }
+  })
+);
