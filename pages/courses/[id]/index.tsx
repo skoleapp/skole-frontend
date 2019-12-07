0ea50514932d2410f758752d579efda1f0d60a68
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-07 18:37:46 +0200

    Update course detail

--- pages/courses/[id]/index.tsx
+++ pages/courses/[id]/index.tsx
@@ -1,4 +1,15 @@
-import { CardHeader, Divider, Typography } from '@material-ui/core';
+import {
+  Avatar,
+  Box,
+  CardContent,
+  CardHeader,
+  Divider,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography
+} from '@material-ui/core';
+import { CloudUploadOutlined, ScoreOutlined } from '@material-ui/icons';
 import moment from 'moment';
 import { NextPage } from 'next';
 import React from 'react';
@@ -9,6 +20,7 @@ import {
   NotFound,
   SlimCardContent,
   StyledCard,
+  StyledList,
   TextLink
 } from '../../../components';
 import { CourseDocument } from '../../../generated/graphql';
@@ -30,38 +42,77 @@ const CourseDetailPage: NextPage<Props> = ({ course }) => {
     const creatorName = creator.username || 'N/A';
     const created = moment(course.created).format('LL');
     const modified = moment(course.modified).format('LL');
+    const points = course.points;
+    const resources = course.resources.length;
+
+    const renderGeneralCourseInfo = (
+      <CardContent>
+        <Box textAlign="left">
+          <Typography variant="body1">Code: {courseCode}</Typography>
+          <Typography variant="body1">
+            Subject:{' '}
+            <TextLink
+              href={{ pathname: '/courses', query: { subjectId: subject.id } }}
+              color="primary"
+            >
+              {subjectName}
+            </TextLink>
+          </Typography>
+          <Typography variant="body1">
+            School:{' '}
+            <TextLink href={`/schools/${school.id}`} color="primary">
+              {schoolName}
+            </TextLink>
+          </Typography>
+          <Typography variant="body1">
+            Creator:{' '}
+            <TextLink href={`/users/${creator.id}`} color="primary">
+              {creatorName}
+            </TextLink>
+          </Typography>
+          <Typography variant="body1">Created: {created}</Typography>
+          <Typography variant="body1">Modified: {modified}</Typography>
+        </Box>
+      </CardContent>
+    );
+
+    const renderCourseInfoList = (
+      <CardContent>
+        <StyledList>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <ScoreOutlined />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText>Points: {points}</ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <CloudUploadOutlined />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText>Resources: {resources}</ListItemText>
+          </ListItem>
+        </StyledList>
+      </CardContent>
+    );
 
     return (
       <Layout heading={fullName} title={fullName} backUrl="/courses">
         <StyledCard>
           <CardHeader title={fullName} />
           <Divider />
-          <SlimCardContent>
-            <Typography variant="body1">Code: {courseCode}</Typography>
-            <Typography variant="body1">
-              Subject:{' '}
-              <TextLink
-                href={{ pathname: '/courses', query: { subjectId: subject.id } }}
-                color="primary"
-              >
-                {subjectName}
-              </TextLink>
-            </Typography>
-            <Typography variant="body1">
-              School:{' '}
-              <TextLink href={`/schools/${school.id}`} color="primary">
-                {schoolName}
-              </TextLink>
-            </Typography>
-            <Typography variant="body1">
-              Creator:{' '}
-              <TextLink href={`/users/${creator.id}`} color="primary">
-                {creatorName}
-              </TextLink>
-            </Typography>
-            <Typography variant="body1">Created: {created}</Typography>
-            <Typography variant="body1">Modified: {modified}</Typography>
-          </SlimCardContent>
+          <Box
+            className="flex-flow"
+            display="flex"
+            justifyContent="space-around"
+            alignItems="center"
+          >
+            {renderGeneralCourseInfo}
+            {renderCourseInfoList}
+          </Box>
           <Divider />
           <SlimCardContent>
             <ButtonLink
