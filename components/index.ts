d2c15b9f095ef25a2f9fa3692c33204f14d632bc
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-20 15:34:14 +0200

    Minor general updates

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,3 @@
-export * from './contact';
 export * from './home';
 export * from './layout';
-export * from './profile';
 export * from './shared';
--- components/layout/NotFound.tsx
+++ components/layout/NotFound.tsx
@@ -1,14 +1,13 @@
 import { CardHeader } from '@material-ui/core';
 import React from 'react';
-import { Layout } from '.';
-import { StyledCard } from '../shared';
+import { Layout, StyledCard } from '../shared';
 
 interface Props {
   title: string;
 }
 
 export const NotFound: React.FC<Props> = ({ title }) => (
-  <Layout title="Not found..." backUrl="/">
+  <Layout title="Not found..." backUrl>
     <StyledCard>
       <CardHeader title={title} />
     </StyledCard>
