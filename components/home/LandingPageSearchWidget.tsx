e278206f051a26e843f4b18fdf65f1f0533a0f6b
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-20 14:47:45 +0200

    Add landing page search widget

--- components/home/LandingPageSearchWidget.tsx
+++ components/home/LandingPageSearchWidget.tsx
@@ -2,7 +2,6 @@ import { Box, Button, InputBase } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
-import { breakpoints } from '../../styles';
 import { useSearch } from '../../utils';
 
 export const LandingPageSearchWidget: React.FC = () => {
@@ -10,13 +9,12 @@ export const LandingPageSearchWidget: React.FC = () => {
 
   return (
     <StyledSearchWidget onSubmit={handleSubmit}>
-      <Box className="flex-flow" display="flex" justifyContent="center">
+      <Box display="flex" justifyContent="center">
         <Box className="search-input">
-          <Search />
           <InputBase placeholder={placeholder} onChange={handleChange} value={searchValue} />
         </Box>
-        <Button type="submit" color="primary" variant="outlined" fullWidth>
-          search
+        <Button type="submit" color="primary" variant="contained">
+          <Search />
         </Button>
       </Box>
     </StyledSearchWidget>
@@ -25,32 +23,19 @@ export const LandingPageSearchWidget: React.FC = () => {
 
 const StyledSearchWidget = styled.form`
   .search-input {
-    border-radius: var(--border-radius);
+    border-radius: var(--border-radius) 0 0 var(--border-radius);
     background-color: var(--white);
     display: flex;
-    align-items: center;
     width: 100%;
     max-width: 20rem;
     border: 0.05rem solid var(--primary);
-    margin: 0.25rem;
-
-    svg {
-      align-self: center;
-      color: var(--primary);
-      margin-left: 0.5rem;
-    }
 
     input {
-      padding: 0.65rem;
+      padding: 0.75rem;
     }
   }
 
   .MuiButton-root {
-    margin: 0.25rem;
-    max-width: 20rem;
-
-    @media only screen and (min-width: ${breakpoints.SM}) {
-      width: auto;
-    }
+    border-radius: 0 var(--border-radius) var(--border-radius) 0;
   }
 `;
