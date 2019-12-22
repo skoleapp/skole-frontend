1b53809202af9468dc792b54f517246f4153cd9a
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-22 10:03:08 +0200

    Fix translations on error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,18 +1,11 @@
-import { CardHeader } from '@material-ui/core';
-import { Layout, StyledCard } from '../components';
+import { NotFound } from '../components';
 import { includeDefaultNamespaces, useTranslation } from '../i18n';
 import { I18nPage } from '../interfaces';
 
 const ErrorPage: I18nPage = () => {
   const { t } = useTranslation();
 
-  return (
-    <Layout title={t('somethingWentWrong')}>
-      <StyledCard>
-        <CardHeader title={t('somethingWentWrong')} />
-      </StyledCard>
-    </Layout>
-  );
+  return <NotFound title={t('common:notFound')} />;
 };
 
 ErrorPage.getInitialProps = () => {
