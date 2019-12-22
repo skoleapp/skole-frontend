b3675de574096d8d388a2007f7d93fff2b0492e3
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-22 14:58:16 +0200

    Update i18n setup

--- i18n.ts
+++ i18n.ts
@@ -4,14 +4,14 @@ import { useTranslation as originalUseTranslation } from 'react-i18next';
 export const nextI18next = new NextI18Next({
   defaultLanguage: 'en',
   otherLanguages: ['fi', 'sv'],
-  localePath: 'public/locales',
+  localePath: typeof window === 'undefined' ? 'public/locales' : 'locales',
   localeSubpaths: { en: 'en', fi: 'fi', sv: 'sv' }
 });
+
+nextI18next.i18n.languages = ['en', 'fi', 'sv'];
 
 export const includeDefaultNamespaces = (namespaces: string[]) =>
   ['common', '_error'].concat(namespaces);
 
-nextI18next.i18n.languages = ['en', 'fi', 'sv'];
-
 export const useTranslation = originalUseTranslation;
-export const { appWithTranslation, Router, i18n, Link } = nextI18next;
+export const { appWithTranslation, Router, Link } = nextI18next;
