14bb47fb88e971c235839d33a3e5c843a224a0a9
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-28 18:11:00 +0200

    Add translations for about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,24 +1,32 @@
 import { CardHeader } from '@material-ui/core';
-import { NextPage } from 'next';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { compose } from 'redux';
 import { Layout, SlimCardContent, StyledCard } from '../components';
+import { includeDefaultNamespaces } from '../i18n';
 import { withApollo, withRedux } from '../lib';
-import { SkoleContext } from '../types';
+import { I18nPage, I18nProps, SkoleContext } from '../types';
 import { useAuthSync } from '../utils';
 
-const AboutPage: NextPage = () => (
-  <Layout heading="About" title="About" backUrl>
-    <StyledCard>
-      <CardHeader title="About" />
-      <SlimCardContent>Here will wil about content...</SlimCardContent>
-    </StyledCard>
-  </Layout>
-);
+const AboutPage: I18nPage = () => {
+  const { t } = useTranslation();
 
-AboutPage.getInitialProps = async (ctx: SkoleContext): Promise<{}> => {
+  return (
+    <Layout title={t('about:title')} backUrl>
+      <StyledCard>
+        <CardHeader title={t('about:title')} />
+        <SlimCardContent>Here will wil about content...</SlimCardContent>
+      </StyledCard>
+    </Layout>
+  );
+};
+
+AboutPage.getInitialProps = async (ctx: SkoleContext): Promise<I18nProps> => {
   await useAuthSync(ctx);
-  return {};
+
+  return {
+    namespacesRequired: includeDefaultNamespaces(['about'])
+  };
 };
 
 export default compose(withApollo, withRedux)(AboutPage);
