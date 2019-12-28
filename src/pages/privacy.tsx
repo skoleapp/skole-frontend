74f0b9034243d3ad5839d0707bc7964cdccfc119
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-28 21:58:10 +0200

    Add translations to privacy page

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,17 +1,32 @@
 import { CardContent, CardHeader } from '@material-ui/core';
-import { NextPage } from 'next';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { compose } from 'redux';
 import { Layout, StyledCard } from '../components';
+import { includeDefaultNamespaces } from '../i18n';
 import { withApollo, withRedux } from '../lib';
+import { I18nPage, I18nProps, SkoleContext } from '../types';
+import { useAuthSync } from '../utils';
 
-const PrivacyPage: NextPage = () => (
-  <Layout title="Privacy" heading="Privacy" backUrl>
-    <StyledCard>
-      <CardHeader title="Skole Privacy Policy" />
-      <CardContent>Here will be privacy policy...</CardContent>
-    </StyledCard>
-  </Layout>
-);
+const PrivacyPage: I18nPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Layout title={t('privacy:title')} backUrl>
+      <StyledCard>
+        <CardHeader title={t('privacy:title')} />
+        <CardContent>Here will be privacy policy...</CardContent>
+      </StyledCard>
+    </Layout>
+  );
+};
+
+PrivacyPage.getInitialProps = async (ctx: SkoleContext): Promise<I18nProps> => {
+  await useAuthSync(ctx);
+
+  return {
+    namespacesRequired: includeDefaultNamespaces(['privacy'])
+  };
+};
 
 export default compose(withRedux, withApollo)(PrivacyPage);
