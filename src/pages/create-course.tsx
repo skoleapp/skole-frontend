0c93a3eb50c481360b9fadaaf4d48868003f1665
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-28 18:29:15 +0200

    Add translations for create course page

--- src/pages/create-course.tsx
+++ src/pages/create-course.tsx
@@ -1,43 +1,58 @@
 import { CardHeader, FormControl, InputLabel, MenuItem } from '@material-ui/core';
 import { ErrorMessage, Field, Formik, FormikProps } from 'formik';
 import { Select, TextField } from 'formik-material-ui';
-import { NextPage } from 'next';
-import { useRouter } from 'next/router';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { compose } from 'redux';
 import * as Yup from 'yup';
 import { CreateCourseInitialDataDocument, useCreateCourseMutation } from '../../generated/graphql';
 import { openNotification } from '../actions';
-import { FormErrorMessage, FormSubmitSection, Layout, SlimCardContent, StyledCard, StyledForm } from '../components';
+import {
+  FormErrorMessage,
+  FormSubmitSection,
+  Layout,
+  SlimCardContent,
+  StyledCard,
+  StyledForm
+} from '../components';
+import { includeDefaultNamespaces, Router } from '../i18n';
 import { withApollo, withRedux } from '../lib';
-import { CreateCourseFormValues, FormCompleted, School, SkoleContext, Subject } from '../types';
+import {
+  CreateCourseFormValues,
+  FormCompleted,
+  I18nPage,
+  I18nProps,
+  School,
+  SkoleContext,
+  Subject
+} from '../types';
 import { useForm, usePrivatePage } from '../utils';
 
-const validationSchema = Yup.object().shape({
-  courseName: Yup.string().required('Course name is required.'),
-  courseCode: Yup.string(),
-  subject: Yup.string().required('Subject is required.'),
-  school: Yup.string().required('School is required.')
-});
-
-interface Props {
+interface Props extends I18nProps {
   subjects?: Subject[];
   schools?: School[];
 }
 
-const CreateCoursePage: NextPage<Props> = ({ subjects, schools }) => {
+const CreateCoursePage: I18nPage<Props> = ({ subjects, schools }) => {
   const { ref, resetForm, setSubmitting, onError } = useForm();
   const dispatch = useDispatch();
-  const router = useRouter();
+  const { t } = useTranslation();
+
+  const validationSchema = Yup.object().shape({
+    courseName: Yup.string().required(t('validation:courseNameRequired')),
+    courseCode: Yup.string(),
+    subject: Yup.string().required(t('validation:subjectRequired')),
+    school: Yup.string().required(t('validation:schoolRequired'))
+  });
 
   const onCompleted = async ({ createCourse }: FormCompleted) => {
     if (!!createCourse.errors) {
       onError(createCourse.errors);
     } else {
       resetForm();
-      dispatch(openNotification('Course created!'));
-      await router.push(`/courses/${createCourse.course.id}`);
+      dispatch(openNotification(t('notifications:courseCreated')));
+      await Router.push(`/courses/${createCourse.course.id}`);
     }
   };
 
@@ -61,20 +76,20 @@ const CreateCoursePage: NextPage<Props> = ({ subjects, schools }) => {
     <StyledForm>
       <Field
         name="courseName"
-        placeholder="Course Name"
-        label="Course Name"
+        placeholder={t('forms:courseName')}
+        label={t('forms:courseName')}
         component={TextField}
         fullWidth
       />
       <Field
         name="courseCode"
-        placeholder="Course Code"
-        label="Course Code"
+        placeholder={t('forms:courseCode')}
+        label={t('forms:courseCode')}
         component={TextField}
         fullWidth
       />
       <FormControl fullWidth>
-        <InputLabel>Subject</InputLabel>
+        <InputLabel>{t('forms:subject')}</InputLabel>
         <Field name="subject" component={Select}>
           <MenuItem value="">---</MenuItem>
           {subjects &&
@@ -87,7 +102,7 @@ const CreateCoursePage: NextPage<Props> = ({ subjects, schools }) => {
         <ErrorMessage name="subject" component={FormErrorMessage} />
       </FormControl>
       <FormControl fullWidth>
-        <InputLabel>School</InputLabel>
+        <InputLabel>{t('forms:school')}</InputLabel>
         <Field name="school" component={Select}>
           <MenuItem value="">---</MenuItem>
           {schools &&
@@ -99,14 +114,14 @@ const CreateCoursePage: NextPage<Props> = ({ subjects, schools }) => {
         </Field>
         <ErrorMessage name="school" component={FormErrorMessage} />
       </FormControl>
-      <FormSubmitSection submitButtonText="save" {...props} />
+      <FormSubmitSection submitButtonText={t('common:submit')} {...props} />
     </StyledForm>
   );
 
   return (
-    <Layout title="Create Course" backUrl>
+    <Layout title={t('create-course:title')} backUrl>
       <StyledCard>
-        <CardHeader title="Create Course" />
+        <CardHeader title={t('create-course:title')} />
         <SlimCardContent>
           <Formik
             initialValues={initialValues}
@@ -127,9 +142,9 @@ CreateCoursePage.getInitialProps = async (ctx: SkoleContext): Promise<Props> => 
 
   try {
     const { data } = await ctx.apolloClient.query({ query: CreateCourseInitialDataDocument });
-    return { ...data };
+    return { ...data, namespacesRequired: includeDefaultNamespaces(['create-course']) };
   } catch {
-    return {};
+    return { namespacesRequired: includeDefaultNamespaces(['create-course']) };
   }
 };
 
