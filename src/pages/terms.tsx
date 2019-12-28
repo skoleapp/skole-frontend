c0d50be05a5623ce65fbcdeae6c5b28dcb7c5d28
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-28 22:13:13 +0200

    Add translations for terms page

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,17 +1,32 @@
 import { CardContent, CardHeader } from '@material-ui/core';
-import { NextPage } from 'next';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { compose } from 'redux';
 import { Layout, StyledCard } from '../components';
+import { includeDefaultNamespaces } from '../i18n';
 import { withApollo, withRedux } from '../lib';
+import { I18nPage, I18nProps, SkoleContext } from '../types';
+import { useAuthSync } from '../utils';
 
-const TermsPage: NextPage = () => (
-  <Layout title="Terms" heading="Terms" backUrl>
-    <StyledCard>
-      <CardHeader title="Skole Terms and Conditions" />
-      <CardContent>Here will be Terms and Conditions...</CardContent>
-    </StyledCard>
-  </Layout>
-);
+const TermsPage: I18nPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Layout title={t('terms:title')} backUrl>
+      <StyledCard>
+        <CardHeader title={t('terms:title')} />
+        <CardContent>Here will be Terms and Conditions...</CardContent>
+      </StyledCard>
+    </Layout>
+  );
+};
+
+TermsPage.getInitialProps = async (ctx: SkoleContext): Promise<I18nProps> => {
+  await useAuthSync(ctx);
+
+  return {
+    namespacesRequired: includeDefaultNamespaces(['terms'])
+  };
+};
 
 export default compose(withApollo, withRedux)(TermsPage);
