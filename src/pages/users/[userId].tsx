ed8a6acc2ec305ef3e7b4852e4eb9528678b0003
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-29 15:25:55 +0200

    FIx user pages

--- src/pages/users/[userId].tsx
+++ src/pages/users/[userId].tsx
@@ -9,48 +9,48 @@ import { I18nPage, I18nProps, SkoleContext, User } from '../../types';
 import { redirect, useAuthSync } from '../../utils';
 
 interface Props extends I18nProps {
-  user?: User;
+    user?: User;
 }
 
 const UserPage: I18nPage<Props> = ({ user }) => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  if (user) {
-    const username = user.username || '-';
+    if (user) {
+        const username = user.username || '-';
 
-    return (
-      <Layout heading={username} title={username} backUrl>
-        <StyledCard>
-          <UserProfileCardContent user={user} />
-        </StyledCard>
-      </Layout>
-    );
-  } else {
-    return <NotFound title={t('profile:notFound')} />;
-  }
+        return (
+            <Layout heading={username} title={username} backUrl>
+                <StyledCard>
+                    <UserProfileCardContent user={user} />
+                </StyledCard>
+            </Layout>
+        );
+    } else {
+        return <NotFound title={t('profile:notFound')} />;
+    }
 };
 
 UserPage.getInitialProps = async (ctx: SkoleContext): Promise<Props> => {
-  const { query, apolloClient } = ctx;
-  const { userMe } = await useAuthSync(ctx);
-  const { userId } = query;
+    const { query, apolloClient } = ctx;
+    const { userMe } = await useAuthSync(ctx);
+    const { userId } = query;
 
-  // Redirect to own profile if id matches signed in user.
-  if (userMe && userMe.id === userId) {
-    redirect(ctx, '/profile');
-    return { namespacesRequired: includeDefaultNamespaces(['profile']) };
-  }
+    // Redirect to own profile if id matches signed in user.
+    if (userMe && userMe.id === userId) {
+        redirect(ctx, '/profile');
+        return { namespacesRequired: includeDefaultNamespaces(['profile']) };
+    }
 
-  try {
-    const { data } = await apolloClient.query({
-      query: UserDocument,
-      variables: { userId }
-    });
+    try {
+        const { data } = await apolloClient.query({
+            query: UserDocument,
+            variables: { userId },
+        });
 
-    return { ...data, namespacesRequired: includeDefaultNamespaces(['profile']) };
-  } catch {
-    return { namespacesRequired: includeDefaultNamespaces(['profile']) };
-  }
+        return { ...data, namespacesRequired: includeDefaultNamespaces(['profile']) };
+    } catch {
+        return { namespacesRequired: includeDefaultNamespaces(['profile']) };
+    }
 };
 
 export default compose(withRedux, withApollo)(UserPage);
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,65 +2,74 @@ import { Avatar, Table, TableBody, TableCell, TableHead, TableRow, Typography } 
 import Link from 'next/link';
 import * as R from 'ramda';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { compose } from 'redux';
 import { UsersDocument } from '../../../generated/graphql';
 import { Layout, StyledTable } from '../../components';
 import { includeDefaultNamespaces } from '../../i18n';
 import { withApollo, withRedux } from '../../lib';
-import { SkoleContext, User } from '../../types';
+import { I18nPage, I18nProps, SkoleContext, User } from '../../types';
 import { useAuthSync } from '../../utils';
-  return (
-    <Layout heading={t('users:title')} title={t('users:title')} backUrl>
-      <StyledTable>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <Typography variant="h6">{t('common:users')}</Typography>
-              </TableCell>
-              <TableCell align="right">
-                <Typography variant="h6">{t('common:points')}</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users && users.length ? (
-              users.map((u: User, i: number) => (
-                <Link href={`/users/${u.id}`} key={i}>
-                  <TableRow>
-                    <TableCell className="user-cell">
-                      <Avatar src={process.env.BACKEND_URL + u.avatarThumbnail} />
-                      <Typography variant="subtitle1">{R.propOr('-', 'username', u)}</Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography variant="subtitle1">{R.propOr('-', 'points', u)}</Typography>
-                    </TableCell>
-                  </TableRow>
-                </Link>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell className="user-cell">
-                  <Typography variant="subtitle1">{t('users:noUsersFound')}</Typography>
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </StyledTable>
-    </Layout>
-  );
+
+interface Props extends I18nProps {
+    users?: User[];
+}
+
+const UsersPage: I18nPage<Props> = ({ users }) => {
+    const { t } = useTranslation();
+
+    return (
+        <Layout heading={t('users:title')} title={t('users:title')} backUrl>
+            <StyledTable>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                <Typography variant="h6">{t('common:users')}</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Typography variant="h6">{t('common:points')}</Typography>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users && users.length ? (
+                            users.map((u: User, i: number) => (
+                                <Link href={`/users/${u.id}`} key={i}>
+                                    <TableRow>
+                                        <TableCell className="user-cell">
+                                            <Avatar src={process.env.BACKEND_URL + u.avatarThumbnail} />
+                                            <Typography variant="subtitle1">{R.propOr('-', 'username', u)}</Typography>
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Typography variant="subtitle1">{R.propOr('-', 'points', u)}</Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                </Link>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell className="user-cell">
+                                    <Typography variant="subtitle1">{t('users:noUsersFound')}</Typography>
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </StyledTable>
+        </Layout>
+    );
 };
 
 UsersPage.getInitialProps = async (ctx: SkoleContext): Promise<Props> => {
-  await useAuthSync(ctx);
+    await useAuthSync(ctx);
 
-  try {
-    const { data } = await ctx.apolloClient.query({ query: UsersDocument });
-    return { ...data, namespacesRequired: includeDefaultNamespaces(['users']) };
-  } catch {
-    return { namespacesRequired: includeDefaultNamespaces(['users']) };
-  }
+    try {
+        const { data } = await ctx.apolloClient.query({ query: UsersDocument });
+        return { ...data, namespacesRequired: includeDefaultNamespaces(['users']) };
+    } catch {
+        return { namespacesRequired: includeDefaultNamespaces(['users']) };
+    }
 };
 
 export default compose(withApollo, withRedux)(UsersPage);
