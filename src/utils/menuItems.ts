c520f59e8de4afc0984c59fa3e98ace34ef9548f
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-30 22:24:43 +0200

    Add menuitems to utils

---
+++ src/utils/menuItems.ts
@@ -0,0 +1,50 @@
+export const menuItems = {
+    account: [
+        {
+            text: 'settings:editProfile',
+            href: '/account/edit-profile',
+        },
+        {
+            text: 'settings:changePassword',
+            href: '/account/change-password',
+        },
+        {
+            text: 'settings:deleteAccount',
+            href: '/account/delete-account',
+        },
+    ],
+    language: [
+        {
+            title: 'languages:english',
+            value: 'en',
+        },
+        {
+            title: 'languages:finnish',
+            value: 'fi',
+        },
+        {
+            title: 'languages:swedish',
+            value: 'sv',
+        },
+    ],
+    about: [
+        {
+            text: 'common:about',
+            href: '/about',
+        },
+        {
+            text: 'common:contact',
+            href: '/contact',
+        },
+    ],
+    legal: [
+        {
+            text: 'common:terms',
+            href: '/terms',
+        },
+        {
+            text: 'common:privacy',
+            href: '/privacy',
+        },
+    ],
+};
