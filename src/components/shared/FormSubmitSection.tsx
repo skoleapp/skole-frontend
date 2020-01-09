7e0b6206c0b5dc96ebe45d61ac4fd86866f0960a
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-01-09 18:02:21 +0200

    Improve form submit section

--- src/components/shared/FormSubmitSection.tsx
+++ src/components/shared/FormSubmitSection.tsx
@@ -1,14 +1,21 @@
-import { Box, Button, CircularProgress, FormControl } from '@material-ui/core';
+import { Box, Button, ButtonProps, CircularProgress, FormControl } from '@material-ui/core';
 import { ErrorMessage, FormikProps } from 'formik';
 
 import { FormErrorMessage } from '.';
 import React from 'react';
+import { Send } from '@material-ui/icons';
 
 interface FormSubmitSectionProps extends FormikProps<{}> {
     submitButtonText: string;
 }
 
-export const FormSubmitSection: React.FC<FormSubmitSectionProps> = ({ isSubmitting, submitButtonText }) => (
+export const FormSubmitSection: React.FC<FormSubmitSectionProps & ButtonProps> = ({
+    isSubmitting,
+    submitButtonText,
+    endIcon,
+    variant,
+    color,
+}) => (
     <FormControl fullWidth>
         <Box display="flex" flexDirection="column" alignItems="center">
             {isSubmitting ? (
@@ -16,7 +23,14 @@ export const FormSubmitSection: React.FC<FormSubmitSectionProps> = ({ isSubmitti
             ) : (
                 <ErrorMessage name="general" component={FormErrorMessage} />
             )}
-            <Button type="submit" disabled={isSubmitting} variant="contained" color="primary" fullWidth>
+            <Button
+                type="submit"
+                disabled={isSubmitting}
+                variant={variant || 'contained'}
+                endIcon={endIcon || <Send />}
+                color={color || 'primary'}
+                fullWidth
+            >
                 {submitButtonText}
             </Button>
         </Box>
