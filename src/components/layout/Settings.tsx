9ff336d90a4dc9c4b0014c8545518b9381ac42b7
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-01-14 20:02:14 +0200

    Add settings modal

---
+++ src/components/layout/Settings.tsx
@@ -0,0 +1,65 @@
+import { Backdrop, Box, Fade, IconButton, Modal, Paper } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { AnyAction } from 'redux';
+import { Close } from '@material-ui/icons';
+import React from 'react';
+import { State } from '../../types';
+import { breakpoints } from '../../styles';
+import styled from 'styled-components';
+import { toggleSettings } from '../../actions';
+import { useSettings } from '../../utils';
+
+export const Settings: React.FC = () => {
+    const { open } = useSelector((state: State) => state.settings);
+    const { renderSettingsCardContent } = useSettings({ modal: true });
+    const dispatch = useDispatch();
+    const handleClose = (): AnyAction => dispatch((toggleSettings(false) as unknown) as AnyAction);
+
+    const renderCloseIcon = (
+        <Box display="flex" justifyContent="flex-end">
+            <IconButton onClick={handleClose}>
+                <Close />
+            </IconButton>
+        </Box>
+    );
+
+    return (
+        <StyledSettings
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{ timeout: 500 }}
+        >
+            <Fade in={open}>
+                <Paper>
+                    {renderCloseIcon}
+                    {renderSettingsCardContent}
+                </Paper>
+            </Fade>
+        </StyledSettings>
+    );
+};
+
+const StyledSettings = styled(Modal)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .MuiPaper-root {
+        outline: none;
+        padding: 0.5rem;
+        height: 100%;
+        width: 100%;
+
+        @media only screen and (max-width: ${breakpoints.MD}) {
+            overflow-y: scroll;
+        }
+
+        @media only screen and (min-width: ${breakpoints.MD}) {
+            height: auto;
+            max-width: 25rem;
+        }
+    }
+`;
