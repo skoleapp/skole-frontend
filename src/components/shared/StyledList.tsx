7b850011b748b654ddff1d7cac318c8032e15a16
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-01-16 17:08:31 +0200

    Update styled list

--- src/components/shared/StyledList.tsx
+++ src/components/shared/StyledList.tsx
@@ -2,10 +2,6 @@ import { List } from '@material-ui/core';
 import styled from 'styled-components';
 
 export const StyledList = styled(List)`
-    .MuiListItem-root {
-        text-align: center;
-    }
-
     .MuiAvatar-root {
         background-color: var(--primary);
     }
