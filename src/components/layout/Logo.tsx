c67ff4e40e3a661b1167b08fb3f13ac291889e54
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-01-17 15:34:39 +0200

    Fix svg logo

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,16 +1,14 @@
 import { Link } from '../../i18n';
 import React from 'react';
-import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
 
 export const Logo: React.FC = () => (
     <Link href="/">
-        <StyledLogo variant="h1">skole</StyledLogo>
+        <StyledLogo src="/images/logo-text.svg" />
     </Link>
 );
 
-const StyledLogo = styled(Typography)`
-    font-family: 'Roboto Mono', monospace !important;
-    color: var(--secondary);
+const StyledLogo = styled.img`
     cursor: pointer;
+    height: 1.5rem;
 `;
