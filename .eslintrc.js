cc74ea981f44925964216f93e44960f147e02267
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-01-21 20:30:46 +0200

    Fix code formatting settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser.
+    parser: '@typescript-eslint/parser',
     extends: [
-        'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react.
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin.
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
     ],
+    plugins: ['eslint-plugin-simple-import-sort'],
     parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features.
-        sourceType: 'module', // Allows for the use of imports.
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off".
         'react/prop-types': 'off',
+        "simple-import-sort/sort": "error",
+        "sort-imports": "off",
+        "import/order": "off",
+        "import/prefer-default-export": "off"
     },
     settings: {
         react: {
-            version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use.
+            version: 'detect',
         },
     },
 };
