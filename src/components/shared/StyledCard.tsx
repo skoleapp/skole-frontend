8ccd673168f1de5742ea2538514ede1f30908ca6
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-01-29 21:39:51 +0200

    Add margin left prop for styled card

--- src/components/shared/StyledCard.tsx
+++ src/components/shared/StyledCard.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const StyledCard = styled(({ scrollable, ...other }) => <Card {...other} />)`
+export const StyledCard = styled(({ scrollable, marginLeft, ...other }) => <Card {...other} />)`
     flex-grow: 1;
     overflow-y: ${({ scrollable }): string => (scrollable ? 'scroll !important' : 'inherit')};
+    margin-left: ${({ marginLeft }): string => (!!marginLeft ? '0.5rem' : '0')};
 
     .main-avatar {
         height: 10rem;
