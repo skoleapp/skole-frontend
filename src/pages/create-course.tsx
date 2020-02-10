6e90810069f1f540d4f62db5962804eaa5673cf9
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-02-10 19:41:43 +0200

    Fix create course page

--- src/pages/create-course.tsx
+++ src/pages/create-course.tsx
@@ -64,8 +64,8 @@ const CreateCoursePage: I18nPage<I18nProps> = () => {
         const variables = {
             courseName,
             courseCode,
-            school: R.propOr('', 'id', school) as string,
-            subject: R.propOr('', 'id', subject) as string,
+            schoolId: R.propOr('', 'id', school) as number,
+            subjectId: R.propOr('', 'id', subject) as number,
         };
 
         createCourseMutation({ variables });
