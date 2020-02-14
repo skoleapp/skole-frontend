6c3f6c3533d8ecfd3ea19a2ce541415c0154dc7a
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-02-14 16:27:30 +0200

    Fix settings layout

--- src/components/templates/SettingsLayout.tsx
+++ src/components/templates/SettingsLayout.tsx
@@ -8,20 +8,13 @@ import { useSettings } from '../../utils';
 export const SettingsLayout: React.FC<LayoutProps> = ({ title, renderCardContent, renderDialog, ...props }) => {
     const { renderSettingsCardContent } = useSettings({ modal: false });
 
-    const renderMobileContent = (
-        <StyledCard className="md-down">
-            <CardHeader title={title} />
-            <CardContent>{renderCardContent}</CardContent>
-        </StyledCard>
-    );
-
-    const renderDesktopContent = (
-        <StyledCard className="md-up">
+    const renderContent = (
+        <StyledCard>
             <Grid container>
-                <Grid item xs={4} lg={3}>
+                <Grid className="md-up" item xs={12} md={4} lg={3}>
                     <CardContent>{renderSettingsCardContent}</CardContent>
                 </Grid>
-                <Grid item xs={8} lg={9} container justify="center">
+                <Grid item xs={12} md={8} lg={9} container justify="center">
                     <Grid item xs={12} sm={8} md={6} lg={5}>
                         <CardHeader title={title} />
                         <CardContent>{renderCardContent}</CardContent>
@@ -33,8 +26,7 @@ export const SettingsLayout: React.FC<LayoutProps> = ({ title, renderCardContent
 
     return (
         <MainLayout title={title} {...props}>
-            {renderMobileContent}
-            {renderDesktopContent}
+            {renderContent}
             {renderDialog}
         </MainLayout>
     );
