3f181cfd31c6e7435afab5138cca4f41310982f1
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-02-19 23:41:02 +0200

    Add top comment in discussion box

--- src/components/shared/DiscussionBox.tsx
+++ src/components/shared/DiscussionBox.tsx
@@ -1,4 +1,5 @@
-import { Box } from '@material-ui/core';
+import { Box, Divider, Typography } from '@material-ui/core';
+import * as R from 'ramda';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
@@ -8,17 +9,31 @@ import { CommentCard } from './CommentCard';
 import { CreateCommentForm } from './CreateCommentForm';
 
 interface Props {
+    commentThread?: CommentObjectType | null;
     comments: CommentObjectType[];
     isThread?: boolean;
     target: CommentTarget;
 }
 
-export const DiscussionBox: React.FC<Props> = ({ comments: initialComments, isThread, target }) => {
+export const DiscussionBox: React.FC<Props> = ({ commentThread, comments: initialComments, isThread, target }) => {
     const [comments, setComments] = useState(initialComments);
     const appendComments = (comment: CommentObjectType): void => setComments([...comments, comment]);
 
+    const renderTopComment = !!commentThread && (
+        <>
+            <CommentCard comment={commentThread} isThread={!!isThread} />
+            <Box padding="0.25rem" display="flex" alignItems="center">
+                <Typography variant="subtitle2" color="textSecondary">
+                    {R.propOr('-', 'replyCount', commentThread)} replies
+                </Typography>
+                <Divider />
+            </Box>
+        </>
+    );
+
     const renderMessageArea = (
         <Box className="message-area">
+            {renderTopComment}
             {comments.map((c: CommentObjectType, i: number) => (
                 <Box key={i}>
                     <CommentCard comment={c} isThread={!!isThread} />
@@ -59,6 +74,11 @@ const StyledDiscussionBox = styled(Box)`
         .message-area {
             flex-grow: 1;
             overflow-y: scroll;
+
+            .MuiDivider-root {
+                flex-grow: 1;
+                margin-left: 0.5rem;
+            }
 
             ::-webkit-scrollbar {
                 display: none;
