49415136cd793e0144c458e700f0b1e9935ae655
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-02-20 19:51:20 +0200

    Allo title on modal header

--- src/components/shared/ModalHeader.tsx
+++ src/components/shared/ModalHeader.tsx
@@ -1,13 +1,15 @@
-import { Box, IconButton } from '@material-ui/core';
+import { Box, IconButton, Typography } from '@material-ui/core';
 import { Close } from '@material-ui/icons';
 import React from 'react';
 
 interface Props {
     onClick: () => void;
+    title?: string;
 }
 
-export const ModalHeader: React.FC<Props> = ({ onClick }) => (
-    <Box display="flex" justifyContent="flex-end" borderBottom="var(--border)">
+export const ModalHeader: React.FC<Props> = ({ onClick, title }) => (
+    <Box display="flex" justifyContent="space-between" alignItems="center" borderBottom="var(--border)">
+        {!!title && <Typography variant="subtitle1">{title}</Typography>}
         <IconButton onClick={onClick}>
             <Close />
         </IconButton>
