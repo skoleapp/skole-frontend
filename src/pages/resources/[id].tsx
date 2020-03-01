cd5261e3cbfa4a840b02647167834a95895c4be1
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-03-01 18:00:19 +0200

    Add tab layout to resource page

--- src/pages/resources/[id].tsx
+++ src/pages/resources/[id].tsx
@@ -5,40 +5,36 @@ import {
     CardHeader,
     Divider,
     Grid,
+    IconButton,
     List,
     ListItem,
     ListItemAvatar,
     ListItemText,
+    Paper,
+    SwipeableDrawer,
     Tab,
     Tabs,
     Typography,
-    Fade,
-    Paper,
-    IconButton,
 } from '@material-ui/core';
-import { CloudDownload, ScoreOutlined, InfoOutlined } from '@material-ui/icons';
+import {
+    CloudUploadOutlined,
+    InfoOutlined,
+    LibraryAddOutlined,
+    SchoolOutlined,
+    ScoreOutlined,
+} from '@material-ui/icons';
 import moment from 'moment';
 import * as R from 'ramda';
 import React, { useState } from 'react';
 import { compose } from 'redux';
 
-import { ResourceDetailDocument, ResourceObjectType, CommentObjectType } from '../../../generated/graphql';
-import {
-    MainLayout,
-    NotFound,
-    StyledCard,
-    TextLink,
-    StyledModal,
-    ModalHeader,
-    ResourcePreview,
-    DiscussionBox,
-    TabPanel,
-} from '../../components';
+import { CommentObjectType, ResourceDetailDocument, ResourceObjectType } from '../../../generated/graphql';
+import { DiscussionBox, MainLayout, ModalHeader, NotFound, StyledCard, TabPanel, TextLink } from '../../components';
 import { useTranslation } from '../../i18n';
 import { includeDefaultNamespaces } from '../../i18n';
 import { withApollo, withRedux } from '../../lib';
 import { I18nPage, I18nProps, SkoleContext } from '../../types';
-import { usePrivatePage, useTabs } from '../../utils';
+import { mediaURL, usePrivatePage, useTabs } from '../../utils';
 
 interface Props extends I18nProps {
     resource?: ResourceObjectType;
@@ -47,149 +43,145 @@ interface Props extends I18nProps {
 const ResourceDetailPage: I18nPage<Props> = ({ resource }) => {
     const { tabValue, handleTabChange } = useTabs();
     const { t } = useTranslation();
-
-    const [pages, setPages]: any[] = useState([]);
-    const [currentPage, setCurrentPage]: any = useState(0);
-
     const [resourceInfoVisible, setResourceInfoVisible] = useState(false);
     const handleOpenResourceInfo = (): void => setResourceInfoVisible(true);
     const handleCloseResourceInfo = (): void => setResourceInfoVisible(false);
 
+    // const [pages, setPages]: any[] = useState([]);
+    // const [currentPage, setCurrentPage]: any = useState(0);
+
     if (resource) {
-        const resourceTitle = R.propOr('-', 'title', resource) as string;
+        const title = R.propOr('-', 'title', resource) as string;
         const resourceType = R.propOr('-', 'resourceType', resource);
-        const resourceCourseId = R.propOr('', 'id', resource.course);
-        const resourceCourseName = R.propOr('-', 'name', resource.course) as string;
-        const resourceSchoolId = R.propOr('', 'id', resource.school);
-        const resourceSchoolName = R.propOr('-', 'name', resource.school) as string;
+        const courseName = R.propOr('-', 'name', resource.course) as string;
+        const schoolId = R.propOr('', 'id', resource.school);
+        const schoolName = R.propOr('-', 'name', resource.school) as string;
         const creatorId = R.propOr('', 'id', resource.user);
         const creatorName = R.propOr('-', 'username', resource.user) as string;
         const created = moment(resource.created).format('LL');
-        const modified = moment(resource.modified).format('LL');
         const points = R.propOr('-', 'points', resource);
-
         const comments = R.propOr([], 'comments', resource) as CommentObjectType[];
+
+        const renderCreatedInfo = (
+            <Box padding="0.5rem" textAlign="left">
+                <Typography variant="body2" color="textSecondary">
+                    {t('common:createdBy')}{' '}
+                    <TextLink href={`/users/${creatorId}`} color="primary">
+                        {creatorName}
+                    </TextLink>{' '}
+                    {created}
+                </Typography>
+            </Box>
+        );
+
+        const renderResourceInfo = (
+            <CardContent>
+                <List>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <CloudUploadOutlined />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body2">
+                                {t('common:resourceType')}: {resourceType}
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <SchoolOutlined />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body2">
+                                {t('common:course')}: {courseName}
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <LibraryAddOutlined />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body2">
+                                {t('common:school')}:{' '}
+                                <TextLink href={`/schools/${schoolId}`} color="primary">
+                                    {schoolName}
+                                </TextLink>
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <ScoreOutlined />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body2">
+                                {t('common:points')}: {points}
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                </List>
+            </CardContent>
+        );
 
         const discussionBoxProps = {
             comments,
             target: { resource: Number(resource.id) },
         };
 
-        const renderResourceInfo = (
-            <Grid container alignItems="center">
-                <Grid item container sm={6} justify="center">
-                    <CardContent>
-                        <Box textAlign="left">
-                            <Typography variant="body2">
-                                {t('common:resourceType')}: {resourceType}
-                            </Typography>
-                            <Typography variant="body2">
-                                {t('common:course')}:{' '}
-                                <TextLink href={`/courses/${resourceCourseId}`} color="primary">
-                                    {resourceCourseName}
-                                </TextLink>
-                            </Typography>
-                            <Typography variant="body2">
-                                {t('common:school')}:{' '}
-                                <TextLink href={`/schools/${resourceSchoolId}`} color="primary">
-                                    {resourceSchoolName}
-                                </TextLink>
-                            </Typography>
-                            <Typography variant="body2">
-                                {t('common:creator')}:{' '}
-                                <TextLink href={`/users/${creatorId}`} color="primary">
-                                    {creatorName}
-                                </TextLink>
-                            </Typography>
-                            <Typography variant="body2">
-                                {t('common:created')}: {created}
-                            </Typography>
-                            <Typography variant="body2">
-                                {t('common:modified')}: {modified}
-                            </Typography>
-                        </Box>
-                    </CardContent>
-                </Grid>
-                <Grid item container sm={6} justify="center">
-                    <CardContent>
-                        <List>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <ScoreOutlined />
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText>
-                                    {t('common:points')}: {points}
-                                </ListItemText>
-                            </ListItem>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <CloudDownload />
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText>{t('common:downloads')}: 0</ListItemText>
-                            </ListItem>
-                        </List>
-                    </CardContent>
-                </Grid>
-            </Grid>
-        );
+        const renderFile = <img src={mediaURL(resource.file)} />;
 
-        const renderResourceInfoModal = (
-            <StyledModal open={!!resourceInfoVisible} onClose={handleCloseResourceInfo}>
-                <Fade in={!!resourceInfoVisible}>
-                    <Paper>
-                        <ModalHeader onCancel={handleCloseResourceInfo} />
-                        <Box textAlign="center">
-                            <CardHeader title={resourceTitle} />
-                            {renderResourceInfo}
-                        </Box>
-                    </Paper>
-                </Fade>
-            </StyledModal>
-        );
-
-        const renderResourceInfoButton = (
-            <IconButton color="secondary" onClick={handleOpenResourceInfo}>
-                <InfoOutlined />
-            </IconButton>
-        );
-
-        const renderContent = (
-            <Grid container>
-                <Grid item container xs={12} sm={12} md={7} lg={8}>
-                    <StyledCard>
-                        <Tabs
-                            className="md-down"
-                            value={tabValue}
-                            onChange={handleTabChange}
-                            indicatorColor="primary"
-                            textColor="primary"
-                            variant="fullWidth"
-                        >
-                            <Tab label={resourceTitle} />
-                            <Tab label={t('common:discussion')} />
-                        </Tabs>
-                        <CardHeader className="md-up" title={resourceTitle} />
-
-                        {tabValue === 0 && (
-                            <ResourcePreview
+        const renderMobileContent = (
+            <Grid container className="md-down">
+                <StyledCard>
+                    <Tabs
+                        value={tabValue}
+                        onChange={handleTabChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        variant="fullWidth"
+                    >
+                        <Tab label={t('common:resource')} />
+                        <Tab label={t('common:discussion')} />
+                    </Tabs>
+                    <TabPanel value={tabValue} index={0}>
+                        {/* <ResourcePreview
                                 currentPage={currentPage}
                                 setCurrentPage={setCurrentPage}
                                 pages={pages}
                                 setPages={setPages}
                                 resource={resource}
-                            />
-                        )}
+                            /> */}
+                        {renderFile}
+                    </TabPanel>
+                    <TabPanel value={tabValue} index={1} flexGrow="1" display="flex">
+                        <DiscussionBox {...discussionBoxProps} />
+                    </TabPanel>
+                </StyledCard>
+            </Grid>
+        );
 
-                        <TabPanel value={tabValue} index={1} flexGrow="1" display={tabValue === 1 ? 'flex' : 'none'}>
-                            <DiscussionBox {...discussionBoxProps} />
-                        </TabPanel>
+        const renderDesktopContent = (
+            <Grid container className="md-up">
+                <Grid item container xs={12} md={7} lg={8}>
+                    <StyledCard>
+                        <CardHeader title={title} />
+                        <Divider />
+                        {renderResourceInfo}
+                        {renderCreatedInfo}
+                        <Divider />
+                        <CardContent>{renderFile}</CardContent>
                     </StyledCard>
                 </Grid>
-                <Grid item container xs={12} sm={12} md={5} lg={4} className="md-up">
+                <Grid item container xs={12} md={5} lg={4}>
                     <StyledCard marginLeft>
                         <CardHeader title={t('common:discussion')} />
                         <Divider />
@@ -199,15 +191,33 @@ const ResourceDetailPage: I18nPage<Props> = ({ resource }) => {
             </Grid>
         );
 
-        return (
-            <MainLayout
-                showBottomNavigation={tabValue === 1}
-                title={resourceTitle}
-                backUrl
-                maxWidth="xl"
-                headerRight={renderResourceInfoButton}
+        const renderResourceInfoButton = (
+            <IconButton color="secondary" onClick={handleOpenResourceInfo}>
+                <InfoOutlined />
+            </IconButton>
+        );
+
+        const renderResourceInfoModal = (
+            <SwipeableDrawer
+                anchor="bottom"
+                open={!!resourceInfoVisible}
+                onOpen={(): void => setResourceInfoVisible(true)}
+                onClose={handleCloseResourceInfo}
             >
-                {renderContent}
+                <Paper>
+                    <ModalHeader onCancel={handleCloseResourceInfo} />
+                    <CardHeader title={title} />
+                    {renderCreatedInfo}
+                    <Divider />
+                    {renderResourceInfo}
+                </Paper>
+            </SwipeableDrawer>
+        );
+
+        return (
+            <MainLayout title={title} backUrl headerRight={renderResourceInfoButton}>
+                {renderMobileContent}
+                {renderDesktopContent}
                 {renderResourceInfoModal}
             </MainLayout>
         );
