b89751e952bb99b9bc77ec29309aa7bb76bd998b
Wiljam Peltomaa <wiljam.peltomaa@epi.fi>
2020-03-01 16:16:06 +0200

    Revert next.config.js changes

--- next.config.js
+++ next.config.js
@@ -2,42 +2,29 @@ const withCSS = require('@zeit/next-css');
 const WebpackBar = require('webpackbar');
 const withAssetsImport = require('next-assets-import');
 
-const withTM = require('next-transpile-modules');
-const withPlugins = require('next-compose-plugins');
+module.exports = withAssetsImport(
+    withCSS({
+        target: 'serverless',
+        env: {
+            API_URL: process.env.API_URL,
+            BACKEND_URL: process.env.BACKEND_URL,
+        },
+        webpack: (config, { dev }) => {
+            if (dev) {
+                config.devtool = '';
+            }
 
-module.exports = withPlugins(
-    [
-        [
-            withTM,
-            {
-                transpileModules: ['ol'],
-            },
-        ],
-    ],
-    withAssetsImport(
-        withCSS({
-            target: 'serverless',
-            env: {
-                API_URL: process.env.API_URL,
-                BACKEND_URL: process.env.BACKEND_URL,
-            },
-            webpack: (config, { dev }) => {
-                if (dev) {
-                    config.devtool = '';
-                }
+            config.plugins.push(new WebpackBar());
 
-                config.plugins.push(new WebpackBar());
+            return config;
+        },
+        webpackDevMiddleware: config => {
+            config.watchOptions = {
+                poll: 1000,
+                aggregateTimeout: 300,
+            };
 
-                return config;
-            },
-            webpackDevMiddleware: config => {
-                config.watchOptions = {
-                    poll: 1000,
-                    aggregateTimeout: 300,
-                };
-
-                return config;
-            },
-        }),
-    ),
+            return config;
+        },
+    }),
 );
