a198bef68be5d961af30d09c389320e2fd303522
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-03-08 17:23:39 +0200

    Add api url behind env variable

--- next.config.js
+++ next.config.js
@@ -3,13 +3,14 @@ const WebpackBar = require('webpackbar');
 const withAssetsImport = require('next-assets-import');
 
 const prod = process.env.NODE_ENV === 'production';
+const API_URL = 'https://api.skoleapp.com/';
 
 module.exports = withAssetsImport(
     withCSS({
         target: 'serverless',
         env: {
-            API_URL: prod ? 'https://api.skoleapp.com/' : 'http://localhost:8000/',
-            BACKEND_URL: prod ? 'https://api.skoleapp.com/' : 'http://backend:8000/',
+            API_URL: prod ? API_URL : 'http://localhost:8000/',
+            BACKEND_URL: prod ? API_URL : 'http://backend:8000/',
         },
         webpack: (config, { dev }) => {
             if (dev) {
