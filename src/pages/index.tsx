0422c68ed1dff97a776f9e311f6ed7f464db6f96
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-03-20 15:40:24 +0200

    Add upload resource icon to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import React from 'react';
 import { compose } from 'redux';
 import styled from 'styled-components';
 
-import { ButtonLink, MainLayout, SettingsButton } from '../components';
+import { ButtonLink, IconButtonLink, MainLayout, SettingsButton } from '../components';
 import { Link, useTranslation } from '../i18n';
 import { includeDefaultNamespaces } from '../i18n';
 import { withApollo, withRedux } from '../lib';
@@ -65,8 +65,16 @@ const IndexPage: I18nPage = () => {
         </form>
     );
 
+    const renderHeaderLeft = <IconButtonLink href="/upload-resource" icon={CloudUploadOutlined} color="secondary" />;
+    const renderHeaderRight = <SettingsButton color="secondary" />;
+
     return (
-        <StyledIndexPage title={t('index:title')} disableSearch headerRight={<SettingsButton color="secondary" />}>
+        <StyledIndexPage
+            title={t('index:title')}
+            disableSearch
+            headerRight={renderHeaderRight}
+            headerLeft={renderHeaderLeft}
+        >
             <Box className="slogan">
                 <Typography variant="h1">{t('index:slogan')}</Typography>
             </Box>
