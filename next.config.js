5cb61b5229772ead30264ee3cc0d0e8b7c871c28
Wiljam Peltomaa <wiljam.peltomaa@epi.fi>
2020-03-26 18:11:06 +0200

    Adapt next-offline plugin to next.config

--- next.config.js
+++ next.config.js
@@ -1,39 +1,67 @@
 const withCSS = require('@zeit/next-css');
 const WebpackBar = require('webpackbar');
 const withAssetsImport = require('next-assets-import');
+const withOffline = require('next-offline');
 
 const prod = process.env.NODE_ENV === 'production';
 const API_URL = 'https://api.skoleapp.com/';
 
-module.exports = withAssetsImport(
-    withCSS({
-        target: 'serverless',
-        env: {
-            API_URL: prod ? API_URL : 'http://localhost:8000/',
-            BACKEND_URL: prod ? API_URL : 'http://backend:8000/',
-        },
-        webpack: (config, { dev }) => {
-            if (dev) {
-                config.devtool = '';
-            }
+module.exports = withOffline(
+    withAssetsImport(
+        withCSS({
+            workboxOpts: {
+                swDest: process.env.NEXT_EXPORT ? 'service-worker.js' : 'static/service-worker.js',
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https?.*/,
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'offlineCache',
+                            expiration: {
+                                maxEntries: 200,
+                            },
+                        },
+                    },
+                ],
+            },
+            experimental: {
+                async rewrites() {
+                    return [
+                        {
+                            source: '/service-worker.js',
+                            destination: '/_next/static/service-worker.js',
+                        },
+                    ];
+                },
+            },
+            target: 'serverless',
+            env: {
+                API_URL: prod ? API_URL : 'http://localhost:8000/',
+                BACKEND_URL: prod ? API_URL : 'http://backend:8000/',
+            },
+            webpack: (config, { dev }) => {
+                if (dev) {
+                    config.devtool = '';
+                }
 
-            config.plugins.push(
-                new WebpackBar({
-                    fancy: true,
-                    profile: true,
-                    basic: false,
-                }),
-            );
+                config.plugins.push(
+                    new WebpackBar({
+                        fancy: true,
+                        profile: true,
+                        basic: false,
+                    }),
+                );
 
-            return config;
-        },
-        webpackDevMiddleware: config => {
-            config.watchOptions = {
-                poll: 1000,
-                aggregateTimeout: 300,
-            };
+                return config;
+            },
+            webpackDevMiddleware: config => {
+                config.watchOptions = {
+                    poll: 1000,
+                    aggregateTimeout: 300,
+                };
 
-            return config;
-        },
-    }),
+                return config;
+            },
+        }),
+    ),
 );
