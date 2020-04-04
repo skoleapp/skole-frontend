3bcf6b1cd732aeaa6267f0404b421eaa407af75a
Wiljam Peltomaa <wiljam.peltomaa@epi.fi>
2020-04-04 18:25:34 +0300

    Add and use morgan (#121)

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { IncomingMessage, ServerResponse } from 'http';
 import next from 'next';
 import nextI18NextMiddleware from 'next-i18next/middleware';
+import morgan from 'morgan';
 
 import { nextI18next } from './i18n';
 
@@ -9,9 +10,19 @@ const port = process.env.PORT || 3001;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
+const logFormatProd =
+    ':remote-addr - :remote-user ":method :url" :status :res[content-length] ":referrer" ":user-agent" :response-time ms';
+const logFormatDev = ':method :url :status :response-time ms - :res[content-length]';
+
 (async (): Promise<void> => {
     await app.prepare();
     const server = express();
+
+    if (process.env.NODE_ENV !== 'production') {
+        server.use(morgan(logFormatDev));
+    } else {
+        server.use(morgan(logFormatProd));
+    }
 
     server.use(nextI18NextMiddleware(nextI18next));
 
