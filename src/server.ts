7747f8cb66b520f9297c7321368741cb2ea88be3
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-04-05 12:41:13 +0300

    Use combined morgan configuration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { IncomingMessage, ServerResponse } from 'http';
+import morgan from 'morgan';
 import next from 'next';
 import nextI18NextMiddleware from 'next-i18next/middleware';
-import morgan from 'morgan';
 
 import { nextI18next } from './i18n';
 
@@ -10,24 +10,12 @@ const port = process.env.PORT || 3001;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
-const logFormatProd =
-    ':remote-addr - :remote-user ":method :url" :status :res[content-length] ":referrer" ":user-agent" :response-time ms';
-const logFormatDev = ':method :url :status :response-time ms - :res[content-length]';
-
 (async (): Promise<void> => {
     await app.prepare();
     const server = express();
-
-    if (process.env.NODE_ENV !== 'production') {
-        server.use(morgan(logFormatDev));
-    } else {
-        server.use(morgan(logFormatProd));
-    }
-
+    server.use(morgan('combined'));
     server.use(nextI18NextMiddleware(nextI18next));
-
     server.get('*', (req: IncomingMessage, res: ServerResponse) => handle(req, res));
-
     await server.listen(port);
     console.log(`Server running on http://localhost:${port}!`);
 })();
