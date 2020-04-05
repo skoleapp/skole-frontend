610ff2658a295b523873664fdb66fabe04e2ff49
Wiljam Peltomaa <wiljam.peltomaa@epi.fi>
2020-04-05 21:29:12 +0300

    Remove unnecessary padding

--- src/components/shared/StyledBottomNavigation.tsx
+++ src/components/shared/StyledBottomNavigation.tsx
@@ -10,7 +10,6 @@ export const StyledBottomNavigation = styled(BottomNavigation)`
     height: calc(var(--safe-area-inset-bottom) + 3rem) !important;
     border-top: var(--border);
     z-index: 1000;
-    padding: 0rem 1rem;
     padding-bottom: var(--safe-area-inset-bottom);
     .MuiButtonBase-root,
     .Mui-selected {
