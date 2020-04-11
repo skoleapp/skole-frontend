7533933b66d708b5ce77a8abb2c7bd4d662f327f
Wiljam Peltomaa <wiljam.peltomaa@epi.fi>
2020-04-11 15:07:32 +0300

    Remove setCurrentMap from redux-actions (not used)

--- src/actions/resource.ts
+++ src/actions/resource.ts
@@ -1,4 +1,3 @@
-import { Map } from 'ol';
 import { Dispatch } from 'react';
 import { AnyAction } from 'redux';
 
@@ -9,7 +8,6 @@ export const PREV_PAGE = 'PREV_PAGE';
 export const NEXT_PAGE = 'NEXT_PAGE';
 export const SET_PAGES = 'SET_PAGES';
 export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
-export const SET_CURRENT_MAP = 'SET_CURRENT_MAP';
 export const RESET_EFFECT = 'RESET_EFFECT';
 
 export const resetEffect = () => (dispatch: Dispatch<AnyAction>): void => {
@@ -34,8 +32,4 @@ export const setPages = (pages: Page[]) => (dispatch: Dispatch<AnyAction>): void
 
 export const setCurrentPage = (currentPage: number) => (dispatch: Dispatch<AnyAction>): void => {
     dispatch({ type: SET_CURRENT_PAGE, payload: currentPage });
-};
-
-export const setCurrentMap = (map: Map) => (dispatch: Dispatch<AnyAction>): void => {
-    dispatch({ type: SET_CURRENT_MAP, payload: map });
 };
