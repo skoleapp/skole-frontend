98297f6954887ced8f75d979e56aa9f80ea7e6ea
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-04-14 21:43:41 +0300

    Fix runtime env vars

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import getConfig from 'next/config';
 
-const { API_URL, BACKEND_URL, CLOUDMERSIVE_API_KEY } = getConfig();
+const { API_URL, BACKEND_URL, CLOUDMERSIVE_API_KEY } = getConfig().publicRuntimeConfig;
 
 export const env = {
     API_URL,
