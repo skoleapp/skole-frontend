805f7528fe1dabbccaf837025ede4d0d6bf21169
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-04-17 00:31:35 +0300

    Clean dependencies

--- src/components/shared/StyledTooltip.tsx
+++ src/components/shared/StyledTooltip.tsx
@@ -1,11 +1,11 @@
 import { Tooltip, TooltipProps, Typography } from '@material-ui/core';
 import React from 'react';
 
-interface Props {
+interface Props extends TooltipProps {
     title: React.ReactNode;
 }
 
-export const StyledTooltip: React.FC<Props & TooltipProps> = ({ children, title, ...props }) => (
+export const StyledTooltip: React.FC<Props> = ({ children, title, ...props }) => (
     <Tooltip PopperProps={{ className: 'md-up' }} {...props} title={<Typography variant="body2">{title}</Typography>}>
         <span>{children}</span>
     </Tooltip>
