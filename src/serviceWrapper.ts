d2ca030719a0d9cc93e24e4ecab7b493f5fe417d
Wiljam Peltomaa <wiljam.peltomaa@epi.fi>
2020-05-02 19:50:28 +0300

    Fix no-unused-vars [ci skip]

--- src/serviceWrapper.ts
+++ src/serviceWrapper.ts
@@ -1,12 +1,13 @@
 // Makke fix this pls
 
-import { useTranslation } from './i18n';
 import { useConfirm } from 'material-ui-confirm';
 import { useEffect } from 'react';
 import { Workbox } from 'workbox-window';
 
+import { useTranslation } from './i18n';
+
 //@ts-ignore
-export const ServiceWrapper = ({ children }: any) => {
+export const ServiceWrapper = ({ children }: any): any => {
     const { t } = useTranslation();
     const confirm = useConfirm();
 
@@ -15,14 +16,14 @@ export const ServiceWrapper = ({ children }: any) => {
         //@ts-ignore
         const workbox: Workbox = window.workbox;
         if (typeof window !== 'undefined' && 'serviceWorker' in navigator && workbox !== undefined) {
-            workbox.addEventListener('waiting', _ => {
+            workbox.addEventListener('waiting', () => {
                 confirm({
                     title: t('common:newVersionAvailable'),
                     confirmationText: t('common:confirmUpdate'),
                     cancellationText: t('common:declineUpdate'),
                 })
                     .then(() => {
-                        workbox.addEventListener('controlling', _ => {
+                        workbox.addEventListener('controlling', () => {
                             window.location.reload();
                         });
                         workbox.messageSW({ type: 'SKIP_WAITING' });
