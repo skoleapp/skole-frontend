0593fa5ee5bfa6e4a05202d6b6daaf3e28b45354
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-05-03 12:35:15 +0300

    Fix native sharing types a bit

--- src/utils/useOptions.tsx
+++ src/utils/useOptions.tsx
@@ -5,21 +5,35 @@ import React, { SyntheticEvent } from 'react';
 
 import { useNotificationsContext } from '../context';
 import { useTranslation } from '../i18n';
-import { ShareNavigator, UseOptions } from '../types';
+import { UseOptions } from '../types';
 import { useDrawer } from './useDrawer';
+
+interface ShareData {
+    title?: string;
+    text?: string;
+    url?: string;
+}
+
+interface ShareNavigator extends Navigator {
+    share?: (data?: ShareData) => Promise<void>;
+}
+
+interface ShareNavigatorWindow extends Window {
+    navigator: ShareNavigator;
+}
 
 export const useOptions = (shareText?: string): UseOptions => {
     const { t } = useTranslation();
     const { toggleNotification } = useNotificationsContext();
     const drawerProps = useDrawer(t('common:options'));
-    const { onClose, renderHeader: renderOptionsHeader } = drawerProps;
+    const { onClose: closeOptions, renderHeader: renderOptionsHeader } = drawerProps;
 
     const handleShare = async (e: SyntheticEvent): Promise<void> => {
-        const shareNavigator: ShareNavigator = window.navigator;
+        const { navigator } = window as ShareNavigatorWindow;
 
-        if (!!shareNavigator && !!shareNavigator.share) {
+        if (!!navigator && !!navigator.share) {
             try {
-                await shareNavigator.share({
+                await navigator.share({
                     title: 'Skole',
                     text: shareText || t('common:slogan'),
                     url: window.location.href,
@@ -29,14 +43,14 @@ export const useOptions = (shareText?: string): UseOptions => {
             } catch {
                 toggleNotification(t('notifications:sharingError'));
             }
-        } else if (!!shareNavigator && !!shareNavigator.clipboard) {
-            shareNavigator.clipboard.writeText(window.location.href);
+        } else if (!!navigator && !!navigator.clipboard) {
+            navigator.clipboard.writeText(window.location.href);
             toggleNotification(t('notifications:linkCopied'));
         } else {
             toggleNotification(t('notifications:sharingError'));
         }
 
-        onClose(e);
+        closeOptions(e);
     };
 
     const renderShareOption = (
