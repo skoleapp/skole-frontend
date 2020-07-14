8a7e02f7dc5ad2c2ee354665b29031fdf6de877d
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-07-14 15:02:13 +0300

    Update server config

--- src/server.ts
+++ src/server.ts
@@ -6,23 +6,20 @@ import nextI18NextMiddleware from 'next-i18next/middleware';
 
 import { nextI18next } from './i18n';
 
+const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3001;
-const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const app = next({ dev });
 const handle = app.getRequestHandler();
+const morganConfig =
+    ':remote-addr - :remote-user ":method :url" :status :res[content-length] ":referrer" ":user-agent" :response-time ms';
 
-app.prepare().then(() => {
+(async (): Promise<void> => {
+    await app.prepare();
     const server = express();
+    await nextI18next.initPromise;
     server.use(nextI18NextMiddleware(nextI18next));
-
-    if (process.env.NODE_ENV === 'production') {
-        server.use(
-            morgan(
-                ':remote-addr - :remote-user ":method :url" :status :res[content-length] ":referrer" ":user-agent" :response-time ms',
-            ),
-        );
-    }
-
-    server.all('*', (req: IncomingMessage, res: ServerResponse) => handle(req, res));
-    server.listen(port);
+    !dev && server.use(morgan(morganConfig));
+    server.get('*', (req: IncomingMessage, res: ServerResponse) => handle(req, res));
+    await server.listen(port);
     console.log(`Server running on http://localhost:${port}!`);
-});
+})();
