75972f4bf5956c879165fdc2a8ff5b2baaf0166e
Eero Ruohola <eero@ruohola.dev>
2020-08-05 21:37:05 +0300

    Use API_URL as BACKEND_URL if the latter doesn't exist

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ const config = {
         dest: 'public',
         register: false,
         skipWaiting: false,
-        disable: prod ? false : true,
+        disable: !prod,
     },
     publicRuntimeConfig: {
         API_URL: process.env.API_URL,
-        BACKEND_URL: process.env.BACKEND_URL,
+        BACKEND_URL: process.env.BACKEND_URL || process.env.API_URL,
         CLOUDMERSIVE_API_KEY: process.env.CLOUDMERSIVE_API_KEY,
     },
     typescript: {
