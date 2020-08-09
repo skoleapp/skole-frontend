3797a9b964854089a469be5fa7860d63f1bc1d98
Markus Blomqvist <markus.blomqvist@anders.fi>
2020-08-09 17:30:02 +0300

    Move meta tags back to custom Head components

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'lib';
 import Head from 'next/head';
 import React from 'react';
+import { colors } from 'styles';
 import { SEOProps } from 'types';
 
 export const HeadComponent: React.FC<SEOProps> = ({ title: customTitle, description: customDescription }) => {
@@ -14,6 +15,19 @@ export const HeadComponent: React.FC<SEOProps> = ({ title: customTitle, descript
             <meta name="description" content={description} />
             <meta property="og:description" content={description} />
             <meta property="og:title" content={title} />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="Skole" />
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0, maximum-scale=1 user-scalable=no viewport-fit=cover"
+            />
+            <meta name="theme-color" content={colors.primary} />
+            <meta content="yes" name="mobile-web-app-capable" />
+            <meta name="apple-mobile-web-app-title" content="Skole" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         </Head>
     );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,19 +28,6 @@ export default class SkoleDocument extends NextDocument {
         return (
             <Html lang="en">
                 <Head>
-                    <meta property="og:type" content="website" />
-                    <meta property="og:site_name" content="Skole" />
-                    <meta charSet="utf-8" />
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1.0, maximum-scale=1 user-scalable=no viewport-fit=cover"
-                    />
-                    <meta name="theme-color" content={colors.primary} />
-                    <meta content="yes" name="mobile-web-app-capable" />
-                    <meta name="apple-mobile-web-app-title" content="Skole" />
-                    <meta name="apple-mobile-web-app-capable" content="yes" />
-                    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                     <link rel="shortcut icon" href="/images/favicon.ico" />
                     <link rel="apple-touch-icon" sizes="180x180" href="images/icons/icon-180x180.png" />
                     <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color={colors.primary} />
