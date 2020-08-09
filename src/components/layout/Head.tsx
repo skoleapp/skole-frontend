3b5c9f904b850c8108432c8ef49f5b1ae4b8c6a7
Markus Blomqvist <markus.blomqvist@anders.fi>
2020-08-09 18:41:30 +0300

    Fix title tags

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -3,24 +3,28 @@ import React from 'react';
 import { colors } from 'styles';
 import { SEOProps } from 'types';
 
-export const HeadComponent: React.FC<SEOProps> = ({ title, description }) => (
-    <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta property="og:description" content={description} />
-        <meta property="og:title" content={title ? `Skole | ${title}` : ''} />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Skole" />
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1 user-scalable=no viewport-fit=cover"
-        />
-        <meta name="theme-color" content={colors.primary} />
-        <meta content="yes" name="mobile-web-app-capable" />
-        <meta name="apple-mobile-web-app-title" content="Skole" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-    </Head>
-);
+export const HeadComponent: React.FC<SEOProps> = ({ title: customTitle, description }) => {
+    const title = customTitle ? `Skole | ${customTitle}` : '';
+
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta property="og:description" content={description} />
+            <meta property="og:title" content={title} />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="Skole" />
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0, maximum-scale=1 user-scalable=no viewport-fit=cover"
+            />
+            <meta name="theme-color" content={colors.primary} />
+            <meta content="yes" name="mobile-web-app-capable" />
+            <meta name="apple-mobile-web-app-title" content="Skole" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        </Head>
+    );
+};
