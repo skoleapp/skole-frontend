212a5b8d5a39b3e0073dcbdf324802390fa9d127
Markus Blomqvist <markus.blomqvist@anders.fi>
2020-08-16 15:17:59 +0300

    Rich text editor WIP

--- src/components/discussion/TextEditor.tsx
+++ src/components/discussion/TextEditor.tsx
@@ -1,6 +1,17 @@
-import { Box, IconButton, Tooltip } from '@material-ui/core';
 import {
-    AssignmentOutlined,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    IconButton,
+    Size,
+    TextField,
+    Tooltip,
+} from '@material-ui/core';
+import {
+    AttachFileOutlined,
     CodeOutlined,
     FormatBoldOutlined,
     FormatItalicOutlined,
@@ -8,27 +19,87 @@ import {
     FormatListNumberedOutlined,
     FormatQuoteOutlined,
     LinkOutlined,
+    SendOutlined,
     StrikethroughSRounded,
 } from '@material-ui/icons';
-import { DraftEditorCommand, DraftHandleValue, Editor, EditorState, RichUtils } from 'draft-js';
-import { useTranslation } from 'lib';
-import React, { useEffect, useRef, useState } from 'react';
+import { useAuthContext } from 'context';
+import {
+    CompositeDecorator,
+    ContentBlock,
+    DraftEditorCommand,
+    DraftHandleValue,
+    Editor,
+    EditorState,
+    RichUtils,
+} from 'draft-js';
+import { FormikProps } from 'formik';
+import { DraftLink, linkStrategy, useTranslation } from 'lib';
+import React, { ChangeEvent, SyntheticEvent, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import { CreateCommentFormValues } from 'types';
 
-// TODO: Finish this and use this instead of the current text field as the form input field.
-export const TextEditor: React.FC = () => {
+interface Props extends FormikProps<CreateCommentFormValues> {
+    handleAttachmentChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    attachment: string | ArrayBuffer | null;
+}
+
+export const TextEditor: React.FC<Props> = ({ setFieldValue, submitForm, handleAttachmentChange, attachment }) => {
+    const decorator = new CompositeDecorator([
+        {
+            strategy: linkStrategy,
+            component: DraftLink,
+        },
+    ]);
+
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty(decorator));
+    const ref = useRef<Editor | null>(null);
+    const focusEditor = (): false | void => !!ref && !!ref.current && ref.current.focus();
     const { t } = useTranslation();
-    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
-    const editor = useRef<Editor>(null);
-    const handleChange = (editorState: EditorState): void => setEditorState(editorState);
-    const focusEditor = (): false | void => !!editor.current && editor.current.focus();
-    const disabled = false;
+    const { verified, verificationRequiredTooltip } = useAuthContext();
+    const contentState = editorState.getCurrentContent();
+    const disabled = !verified;
+    const selection = editorState.getSelection();
+    const selectionCollapsed = selection.isCollapsed();
+    const textContent = editorState.getCurrentContent().getPlainText('\u0001');
+    const attachmentInputRef = useRef<HTMLInputElement | null>(null);
+    const handleUploadAttachment = (): false | void =>
+        !!attachmentInputRef.current && attachmentInputRef.current.click();
+
+    // const inputTooltip = !!verificationRequiredTooltip ? verificationRequiredTooltip : '';
+    const submitButtonTooltip = !!verificationRequiredTooltip ? verificationRequiredTooltip : t('tooltips:sendMessage');
+    const attachmentButtonTooltip = !!verificationRequiredTooltip
+        ? verificationRequiredTooltip
+        : t('tooltips:attachFile');
+
+    const [focused, setFocused] = useState(false);
+    const onFocus = (): void => setFocused(true);
+    const onBlur = (): void => setFocused(false);
+
+    // If the user changes block type before entering any text, we hide the placeholder.
+    // Placeholder is also hidden whenever editor is focused.
+    const hidePlaceholder =
+        (!contentState.hasText() &&
+            contentState
+                .getBlockMap()
+                .first()
+                .getType() !== 'unstyled') ||
+        focused;
+
+    const blockType = editorState
+        .getCurrentContent()
+        .getBlockForKey(selection.getStartKey())
+        .getType();
+
+    const handleChange = (editorState: EditorState): void => {
+        setEditorState(editorState);
+        setFieldValue('text', textContent);
+    };
+
     const [isMac, setIsMac] = useState<boolean | null>(null);
     const actionKey = isMac ? '⌘' : 'Ctrl';
     const hotkeyBold = `${actionKey} + B`;
     const hotkeyItalic = `${actionKey} + I`;
     const hotkeyStrikethrough = `${actionKey} + Shift + X`;
-    const hotkeyCode = `${actionKey} + Shift + C`;
     const hotkeyLink = `${actionKey} + Shift + U`;
     const hotkeyOrderedList = `${actionKey} + Shift + 7`;
     const hotkeyBulletedList = `${actionKey} + Shift + 8`;
@@ -49,18 +120,11 @@ export const TextEditor: React.FC = () => {
         {
             tooltip: t('tooltips:strikeThrough', { hotkeyStrikethrough }),
             icon: StrikethroughSRounded,
-            style: '',
+            style: 'STRIKETHROUGH',
         },
-        {
-            tooltip: t('tooltips:code', { hotkeyCode }),
-            icon: CodeOutlined,
-            style: '',
-        },
-        {
-            tooltip: t('tooltips:link', { hotkeyLink }),
-            icon: LinkOutlined,
-            style: '',
-        },
+    ];
+
+    const blockTypes = [
         {
             tooltip: t('tooltips:orderedList', { hotkeyOrderedList }),
             icon: FormatListNumberedOutlined,
@@ -78,13 +142,17 @@ export const TextEditor: React.FC = () => {
         },
         {
             tooltip: t('tooltips:codeBlock', { hotkeyCodeBlock }),
-            icon: AssignmentOutlined,
-            style: 'CODE',
+            icon: CodeOutlined,
+            style: 'code-block',
         },
     ];
 
+    const commonToolbarButtonProps = {
+        size: 'small' as Size,
+        disabled: disabled,
+    };
+
     useEffect(() => {
-        focusEditor();
         setIsMac(navigator.userAgent.indexOf('Mac OS X') !== -1);
     }, []);
 
@@ -99,43 +167,227 @@ export const TextEditor: React.FC = () => {
         return 'not-handled';
     };
 
-    const toggleInlineStyle = (style: string) => (): void =>
-        handleChange(RichUtils.toggleInlineStyle(editorState, style));
+    const toggleInlineStyle = (style: string) => (e: SyntheticEvent): void => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleInlineStyle(editorState, style));
+    };
+
+    const toggleBlockStyle = (blockType: string) => (e: SyntheticEvent): void => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, blockType));
+    };
+
+    const getBlockStyle = (block: ContentBlock): string => {
+        switch (block.getType()) {
+            case 'blockquote': {
+                return 'RichEditor-blockquote';
+            }
+
+            default: {
+                return '';
+            }
+        }
+    };
+
+    const [URLInputOpen, setURLInputOpen] = useState(false);
+    const [URL, setURL] = useState('');
+
+    const handleCloseURLInput = (): void => {
+        setURLInputOpen(false);
+        focusEditor();
+    };
+
+    const handleLinkInputChange = (e: ChangeEvent<HTMLInputElement>): void => setURL(e.target.value);
+
+    const handleLinkPrompt = (e: SyntheticEvent): void => {
+        e.preventDefault();
+
+        if (!selectionCollapsed) {
+            const startKey = selection.getStartKey();
+            const startOffset = selection.getStartOffset();
+            const blockWithLinkAtBeginning = contentState.getBlockForKey(startKey);
+            const linkKey = blockWithLinkAtBeginning.getEntityAt(startOffset);
+
+            let url = '';
+
+            if (linkKey) {
+                const linkInstance = contentState.getEntity(linkKey);
+                url = linkInstance.getData().url;
+            }
+
+            setURLInputOpen(true);
+            setURL(url);
+        }
+    };
+
+    const confirmLink = (): void => {
+        const contentStateWithEntity = contentState.createEntity('LINK', 'MUTABLE', { url: URL });
+        const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+        const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
+        setEditorState(RichUtils.toggleLink(newEditorState, newEditorState.getSelection(), entityKey));
+        setURLInputOpen(false);
+        setURL('');
+        setTimeout(() => focusEditor(), 0);
+    };
 
     const renderTextField = (
-        <Editor ref={editor} editorState={editorState} onChange={handleChange} handleKeyCommand={handleKeyCommand} />
+        <Editor
+            ref={ref}
+            editorState={editorState}
+            onChange={handleChange}
+            handleKeyCommand={handleKeyCommand}
+            blockStyleFn={getBlockStyle}
+            placeholder={t('common:createComment') + '...'}
+            onFocus={onFocus}
+            onBlur={onBlur}
+        />
+    );
+
+    const renderInlineStyles = inlineStyles.map(({ tooltip, icon: Icon, style }, i) => (
+        <Tooltip key={i} title={tooltip}>
+            <span>
+                <IconButton
+                    {...commonToolbarButtonProps}
+                    onMouseDown={toggleInlineStyle(style)}
+                    color={editorState.getCurrentInlineStyle().has(style) ? 'primary' : 'default'}
+                >
+                    <Icon />
+                </IconButton>
+            </span>
+        </Tooltip>
+    ));
+
+    const renderLinkButton = (
+        <Tooltip title={t('tooltips:link', { hotkeyLink })}>
+            <span>
+                <IconButton
+                    {...commonToolbarButtonProps}
+                    disabled={disabled || selectionCollapsed}
+                    onMouseDown={handleLinkPrompt}
+                >
+                    <LinkOutlined />
+                </IconButton>
+            </span>
+        </Tooltip>
+    );
+
+    const renderURLInput = (
+        <Dialog open={URLInputOpen} onClose={handleCloseURLInput}>
+            <DialogTitle>{t('forms:addLink')}</DialogTitle>
+            <DialogContent>
+                <TextField
+                    value={URL}
+                    onChange={handleLinkInputChange}
+                    label={t('forms:link')}
+                    variant="outlined"
+                    autoFocus
+                    fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleCloseURLInput}>{t('common:cancel')}</Button>
+                <Button onClick={confirmLink} color="primary">
+                    {t('common:confirm')}
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+
+    const renderBlockStyles = blockTypes.map(({ tooltip, icon: Icon, style }, i) => (
+        <Tooltip key={i} title={tooltip}>
+            <span>
+                <IconButton
+                    {...commonToolbarButtonProps}
+                    onMouseDown={toggleBlockStyle(style)}
+                    color={style === blockType ? 'primary' : 'default'}
+                >
+                    <Icon />
+                </IconButton>
+            </span>
+        </Tooltip>
+    ));
+
+    const renderAttachmentButton = (
+        <>
+            <input
+                ref={attachmentInputRef}
+                value=""
+                accept=".png, .jpg, .jpeg"
+                type="file"
+                onChange={handleAttachmentChange}
+                disabled={disabled}
+            />
+            <Tooltip title={attachmentButtonTooltip}>
+                <span>
+                    <IconButton onClick={handleUploadAttachment} {...commonToolbarButtonProps}>
+                        <AttachFileOutlined />
+                    </IconButton>
+                </span>
+            </Tooltip>
+        </>
+    );
+
+    const renderSendButton = (
+        <Box marginLeft="auto">
+            <Tooltip title={submitButtonTooltip}>
+                <span>
+                    <IconButton
+                        {...commonToolbarButtonProps}
+                        onClick={submitForm}
+                        color={!disabled && (!!textContent || !!attachment) ? 'primary' : 'default'}
+                        disabled={disabled || (!textContent && !attachment)} // Require either text content or an attachment.
+                    >
+                        <SendOutlined />
+                    </IconButton>
+                </span>
+            </Tooltip>
+        </Box>
     );
 
     const renderToolbar = (
         <Box marginTop="0.25rem" display="flex">
-            {inlineStyles.map(({ tooltip, icon: Icon, style }, i) => (
-                <Tooltip key={i} title={tooltip}>
-                    <span>
-                        <IconButton
-                            onClick={toggleInlineStyle(style)}
-                            size="small"
-                            disabled={disabled}
-                            color={editorState.getCurrentInlineStyle().has(style) ? 'primary' : 'default'}
-                        >
-                            <Icon />
-                        </IconButton>
-                    </span>
-                </Tooltip>
-            ))}
+            {renderInlineStyles}
+            {renderLinkButton}
+            {renderBlockStyles}
+            {renderAttachmentButton}
+            {renderSendButton}
         </Box>
     );
 
     return (
-        <StyledTextEditor>
+        <StyledTextEditor hidePlaceholder={hidePlaceholder}>
             {renderTextField}
             {renderToolbar}
+            {renderURLInput}
         </StyledTextEditor>
     );
 };
 
-const StyledTextEditor = styled(Box)`
+// Ignore: hidePlaceholder must be omitted from Box props.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const StyledTextEditor = styled(({ hidePlaceholder, ...props }) => <Box {...props} />)`
+    width: 100%;
+    word-break: break-all;
+
     .DraftEditor-editorContainer {
         max-height: 5rem;
         overflow-y: auto;
+        padding: 0.25rem;
+
+        .RichEditor-blockquote {
+            border-left: 0.5rem solid #eee;
+            color: #666;
+            padding: 0.25rem;
+        }
+
+        .public-DraftStyleDefault-pre {
+            background-color: rgba(0, 0, 0, 0.05);
+            padding: 0.5rem;
+            margin: 0;
+        }
+    }
+
+    .public-DraftEditorPlaceholder-root {
+        display: ${({ hidePlaceholder }): false | string => hidePlaceholder && 'none'};
     }
 `;
