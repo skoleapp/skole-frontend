81bd7aaf61a771be20803713408eb142a0b9fc1a
Markus Blomqvist <markus.blomqvist@anders.fi>
2020-08-21 18:29:32 +0300

    Add global custom classes using MUI styles

---
+++ src/styles/custom.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useCustomStyles = makeStyles({
+    mainAvatar: {
+        height: '8rem',
+        width: '8rem',
+    },
+    avatarThumbnail: {
+        height: '1.5rem',
+        width: '1.5rem',
+    },
+});
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,2 @@
-export * from './base';
-export * from './globalStyle';
-export * from './nprogress';
 export * from './theme';
-export * from './colors';
-export * from './breakpoints';
+export * from './custom';
