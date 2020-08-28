a4739f66e0bb027ce639e3a1d74084cb43bbf4ed
Markus Blomqvist <markus.blomqvist@anders.fi>
2020-08-28 19:22:50 +0300

    Add some global overrides in theme configuration

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,55 +1,132 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
 
-import { breakpointsNum } from './breakpoints';
-import { colors } from './colors';
+const colors = {
+    primary: '#ad3636',
+    secondary: '#faf2de',
+};
 
-export const theme = createMuiTheme({
+export const spacing = (factor: number): string => `${0.25 * factor}rem`;
+export const borderRadius = '1.75rem';
+export const border = `0.05rem solid ${grey[300]}`;
+
+let theme = createMuiTheme({
     props: {
         MuiContainer: {
             maxWidth: 'xl',
         },
+        MuiFormControl: {
+            margin: 'dense',
+            fullWidth: true,
+            variant: 'outlined',
+        },
+        MuiTextField: {
+            fullWidth: true,
+            variant: 'outlined',
+        },
+        MuiSelect: {
+            fullWidth: true,
+            variant: 'outlined',
+        },
     },
-    breakpoints: {
-        values: {
-            xs: breakpointsNum.XS,
-            sm: breakpointsNum.SM,
-            md: breakpointsNum.MD,
-            lg: breakpointsNum.LG,
-            xl: breakpointsNum.XL,
+    overrides: {
+        MuiButton: {
+            root: {
+                borderRadius,
+            },
+        },
+        MuiLink: {
+            root: {
+                '&:hover': {
+                    cursor: 'pointer',
+                },
+            },
+        },
+        MuiOutlinedInput: {
+            root: {
+                borderRadius,
+            },
+        },
+        MuiList: {
+            root: {
+                padding: 0,
+            },
+        },
+        MuiListItemAvatar: {
+            root: {
+                width: '2rem',
+                height: '2rem',
+            },
+        },
+        MuiSvgIcon: {
+            root: {
+                width: '1.35rem',
+                height: '1.35rem',
+            },
+        },
+        // MuiListItemText: {
+        //     root: {
+        //         display: "flex",
+        //         alignItems: "center"
+        //             .MuiSvgIcon-root {
+        //                 margin-right: 0.5rem;
+        //             }
+        //     }
+        // },
+        MuiDrawer: {
+            paperAnchorLeft: {
+                minWidth: '20rem',
+            },
+        },
+        MuiCssBaseline: {
+            '@global': {
+                '.main-avatar': {
+                    height: '8rem',
+                    width: '8rem',
+                    margin: spacing(2),
+                },
+                '.avatar-thumbnail': {
+                    height: '1.5rem',
+                    width: '1.5rem',
+                },
+                '.truncate-text': {
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                },
+                '.border-bottom': {
+                    borderBottom: border,
+                },
+                // '.border-top': {
+                //     borderTop: border,
+                // },
+                // "input[type='file']": {
+                //     display: 'none',
+                // },
+                // '.custom-header': {
+                //     height: '3rem',
+                //     padding: spacing(2),
+                //     borderBottom: border,
+                // },
+            },
         },
     },
     palette: {
-        common: {
-            black: colors.black,
-            white: colors.white,
-        },
         primary: {
             main: colors.primary,
-            light: colors.primaryLight,
-            dark: colors.primaryDark,
-            contrastText: colors.white,
         },
         secondary: {
             main: colors.secondary,
-            light: colors.secondaryLight,
-            dark: colors.secondaryDark,
-            contrastText: colors.black,
-        },
-        error: {
-            main: colors.error,
-        },
-        warning: {
-            main: colors.warning,
-        },
-        info: {
-            main: colors.info,
-        },
-        success: {
-            main: colors.success,
         },
     },
     typography: {
-        fontSize: 14,
         fontFamily: ['Roboto', 'sans-serif'].join(','),
+        h1: {
+            fontSize: 50,
+        },
     },
+    spacing,
 });
+
+theme = responsiveFontSizes(theme);
+export { theme };
