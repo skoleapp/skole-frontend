8043e3b809f7890e16761676f5ca65ee126f7056
Markus Blomqvist <markus.blomqvist@anders.fi>
2020-09-15 19:40:08 +0300

    Remove custom styled components

--- src/components/styled-components/StyledTabs.tsx
+++
@@ -1,9 +0,0 @@
-import { Tabs } from '@material-ui/core';
-import React from 'react';
-import styled from 'styled-components';
-
-export const StyledTabs = styled(props => (
-    <Tabs textColor="primary" variant="fullWidth" indicatorColor="primary" {...props} />
-))`
-    border-bottom: var(--border);
-`;
