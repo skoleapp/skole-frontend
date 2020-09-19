263a5a0048b2d61a1de538377c3de380e8b33245
Wiljam Peltomaa <wiljam.peltomaa@congrid.fi>
2020-09-19 17:07:35 +0300

    Remove useMoment.ts, add useDayjs.ts

---
+++ src/hooks/useDayjs.ts
@@ -0,0 +1,21 @@
+import { useTranslation } from 'lib';
+
+import 'dayjs/locale/fi';
+import 'dayjs/locale/sv';
+import 'dayjs/locale/en';
+import dayjs from 'dayjs';
+import localizableFormat from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(localizableFormat);
+dayjs.extend(relativeTime);
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const useDayjs = (date: string | null) => {
+    const { i18n } = useTranslation();
+
+    if (!!date) {
+        return dayjs(date).locale(i18n.language);
+    }
+    return dayjs().locale(i18n.language);
+};
