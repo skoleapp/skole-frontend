6a7b865b11cd656327b7cda5ec68d029107abe9a
Wiljam Peltomaa <wiljam.peltomaa@congrid.fi>
2020-09-19 20:05:34 +0300

    Course resources filtered by resource types

--- src/components/tables/ResourceTableBody.tsx
+++ src/components/tables/ResourceTableBody.tsx
@@ -1,30 +1,86 @@
-import { TableBody, TableCell, TableRow, Typography } from '@material-ui/core';
-import { ResourceObjectType } from 'generated';
+import { TableBody, TableCell, TableRow, Typography, Box } from '@material-ui/core';
+import { ResourceObjectType, ResourceTypeObjectType } from 'generated';
 import { Link } from 'lib';
 import * as R from 'ramda';
 import React from 'react';
 import { urls } from 'utils';
+import { TextVariant, TextColor } from 'types';
 
 interface Props {
     resources: ResourceObjectType[];
+    resourceTypes: ResourceTypeObjectType[];
+}
+const headerProps = {
+    variant: 'body2' as TextVariant,
+    color: 'textSecondary' as TextColor,
+};
+
+interface ResourceObjectTypePairs {
+    [key: string]: ResourceObjectType[] | [];
 }
 
-export const ResourceTableBody: React.FC<Props> = ({ resources }) => (
-    <TableBody>
-        {resources.map((r: ResourceObjectType, i: number) => (
-            <Link href={urls.resource} as={`/resources/${r.id}`} key={i}>
-                <TableRow>
-                    <TableCell>
-                        <Typography variant="body2">{R.propOr('-', 'title', r)}</Typography>
-                        <Typography variant="body2" color="textSecondary">
-                            {R.propOr('-', 'date', r)}
-                        </Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                        <Typography variant="body2">{R.propOr('-', 'score', r)}</Typography>
-                    </TableCell>
-                </TableRow>
-            </Link>
-        ))}
-    </TableBody>
-);
+export const ResourceTableBody: React.FC<Props> = ({ resources, resourceTypes }) => {
+    let filteredByResourceTypes: ResourceObjectTypePairs = resourceTypes.reduce((acc, type) => {
+        if (!!type && !!type.name) {
+            acc = { ...acc, [type.name]: [] };
+        }
+        return acc;
+    }, {});
+
+    resources.forEach(resource => {
+        if (!!resource && !!resource.resourceType && !!filteredByResourceTypes[resource.resourceType]) {
+            filteredByResourceTypes = {
+                ...filteredByResourceTypes,
+                [resource.resourceType]: [...filteredByResourceTypes[resource.resourceType], resource],
+            };
+        }
+    });
+
+    return (
+        <TableBody>
+            {Object.entries(filteredByResourceTypes).reduce((acc: JSX.Element[], currentValue) => {
+                const resourceArray: ResourceObjectType[] = currentValue[1];
+                const resourceType: string = currentValue[0];
+
+                const renderResources =
+                    !!resourceArray.length &&
+                    resourceArray.map((r: ResourceObjectType, i: number) => (
+                        <Link href={urls.resource} as={`/resources/${r.id}`} key={i}>
+                            <TableRow>
+                                <TableCell>
+                                    <Box paddingLeft="1rem">
+                                        <Typography variant="body2">{R.propOr('-', 'title', r)}</Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            {R.propOr('-', 'date', r)}
+                                        </Typography>
+                                    </Box>
+                                </TableCell>
+                                <TableCell align="right">
+                                    <Box paddingLeft="1rem">
+                                        <Typography variant="body2">{R.propOr('-', 'score', r)}</Typography>
+                                    </Box>
+                                </TableCell>
+                            </TableRow>
+                        </Link>
+                    ));
+                if (!!renderResources) {
+                    const renderRowWithHeader = (
+                        <React.Fragment key={resourceType}>
+                            <TableRow hover={false}>
+                                <TableCell>
+                                    <Box paddingLeft="0.5rem">
+                                        <Typography {...headerProps}>{resourceType}</Typography>
+                                    </Box>
+                                </TableCell>
+                            </TableRow>
+                            {renderResources}
+                        </React.Fragment>
+                    );
+                    acc.push(renderRowWithHeader);
+                }
+
+                return acc;
+            }, [])}
+        </TableBody>
+    );
+};
