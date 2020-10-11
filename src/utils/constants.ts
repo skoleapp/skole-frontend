fc3c320e6562abf01b775da5a44941e093c88ff6
Markus Blomqvist <markus.blomqvist@anders.fi>
2020-10-11 21:54:02 +0300

    Rename "Add Courses" to "Add a Course"

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -68,7 +68,7 @@ export const HOME_PAGE_SHORTCUTS = [
         href: urls.uploadResource,
     },
     {
-        text: 'index:addCourses',
+        text: 'index:addCourse',
         icon: SchoolOutlined,
         href: urls.createCourse,
     },
