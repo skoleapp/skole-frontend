4bf7d0334f0813de7dc7c8ae56497607748e47df
Markus Blomqvist <markus.blomqvist@anders.fi>
2020-10-13 20:31:52 +0300

    Use query options for userMe query

--- src/hooks/useUserMe.ts
+++ src/hooks/useUserMe.ts
@@ -3,6 +3,8 @@ import { UserMeQueryHookResult, UserObjectType, useUserMeQuery } from 'generated
 import * as R from 'ramda';
 import { useEffect } from 'react';
 
+import { useQueryOptions } from './useQueryOptions';
+
 interface UseUserMeQuery extends Omit<UserMeQueryHookResult, 'data' | 'loading' | 'error'> {
     userMe: UserObjectType | null;
     authLoading: boolean;
@@ -10,7 +12,8 @@ interface UseUserMeQuery extends Omit<UserMeQueryHookResult, 'data' | 'loading' 
 }
 
 export const useUserMe = (): UseUserMeQuery => {
-    const { data, loading: authLoading, error, ...result } = useUserMeQuery();
+    const queryOptions = useQueryOptions();
+    const { data, loading: authLoading, error, ...result } = useUserMeQuery(queryOptions);
     const { setUserMe, setAuthNetworkError } = useAuthContext();
     const userMe = R.propOr(null, 'userMe', data) as UserObjectType | null;
     const authNetworkError = !!R.propOr(false, 'networkError', error); // We only care about about network error.
