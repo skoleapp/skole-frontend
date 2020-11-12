11bb736ecc25f855509a7d45b0872b19062cead7
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-11-12 18:30:29 +0200

    Fix comment card actions button spacing

--- src/components/discussion/CommentCard.tsx
+++ src/components/discussion/CommentCard.tsx
@@ -71,9 +71,9 @@ const useStyles = makeStyles(({ spacing }) => ({
         position: 'absolute',
         left: 0,
         right: 0,
+        bottom: 0,
         margin: '0 auto',
         width: '2.5rem',
-        bottom: spacing(2),
     },
 }));
 
