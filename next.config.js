89341b436b7a09142ac84a5057b2881c3b442fad
Eero Ruohola <eero@ruohola.dev>
2020-11-15 13:16:51 +0200

    Fix memory leak that has forever been occurring in prod
    The 'serverless' target would misbehave with a normal containerized
    deployment in EC2. On the first time a unique page was loaded it would
    take a new 28MB chunk from it into memory and hold it there seemingly
    indefinitely. This would cause the memory usage to spike up over time a
    lot, basically without a bound. With this change, the memory usage of
    the production server locally would seem to be at around 200-300MB, even
    with moderate use.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withOffline = require('next-offline');
 const { locales, defaultLocale } = require('./i18n.json');
 
 const config = {
-    target: 'serverless',
+    target: 'server',
     env: {
         API_URL: process.env.API_URL,
         BACKEND_URL: process.env.BACKEND_URL || process.env.API_URL,
