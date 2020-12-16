0e043d57fbb1ea5e9b28ecbe1d2bf8f61d3247ef
Markus Blomqvist <markus@skoleapp.com>
2020-12-16 11:55:07 +0200

    Refactor pagination helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -17,31 +17,23 @@ interface QueryWithPaginationProps {
   extraFilters: Record<symbol, unknown>;
 }
 
-// A utility that we use to get query parameters *with* pagination included.
-// So Use this when you need to update the query params and retain the pagination query.
+// Get query parameters with pagination included.
 export const getQueryWithPagination = ({
   query,
   extraFilters,
 }: QueryWithPaginationProps): ParsedUrlQueryInput =>
-  R.pickBy(
-    (val: string, key: string): boolean =>
-      (!!val && key === 'page') || key === 'pageSize' || key in extraFilters,
-    query,
-  );
+  R.pick(['page', 'pageSize', ...Object.keys(extraFilters)], query);
 
-// A utility that we use to get *only* the pagination query.
-// This will lose all other query params so only use this when you want to reset all other query params except the pagination ones.
+// Get pagination params and drop all other params.
 export const getPaginationQuery = (query: ParsedUrlQueryInput): ParsedUrlQueryInput =>
-  R.pickBy(
-    (val: string, key: string): boolean => (!!val && key === 'page') || key === 'pageSize',
-    query,
-  );
+  R.pick(['page', 'pageSize'], query);
 
 // Truncate strings to a given length.
 export const truncate = (str: string, num: number): string => {
   if (str.length > num) {
     return `${str.slice(0, num)}...`;
   }
+
   return str;
 };
 
