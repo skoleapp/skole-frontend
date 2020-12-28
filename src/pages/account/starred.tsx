1f551e1a32dc1d277ce832b8f4bd2eebd861913e
Markus Blomqvist <markus@skoleapp.com>
2020-12-28 11:15:17 +0200

    Change layout for starred page to a non-settings page

--- src/pages/account/starred.tsx
+++ src/pages/account/starred.tsx
@@ -1,29 +1,47 @@
-import { Tab, Tabs } from '@material-ui/core';
+import { CardHeader, makeStyles, Paper, Tab, Tabs } from '@material-ui/core';
 import {
   CourseTableBody,
   ErrorTemplate,
   LoadingBox,
+  MainTemplate,
   NotFoundBox,
   NotFoundTemplate,
   OfflineTemplate,
   PaginatedTable,
   ResourceTableBody,
-  SettingsTemplate,
   TabPanel,
 } from 'components';
 import { useAuthContext } from 'context';
 import { useStarredQuery } from 'generated';
 import { withAuth } from 'hocs';
-import { useLanguageHeaderContext, useTabs } from 'hooks';
+import { useLanguageHeaderContext, useMediaQueries, useTabs } from 'hooks';
 import { loadNamespaces, useTranslation } from 'lib';
 import { GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import * as R from 'ramda';
 import React from 'react';
+import { BORDER, BORDER_RADIUS } from 'theme';
+
+const useStyles = makeStyles(({ breakpoints }) => ({
+  root: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    overflow: 'hidden',
+    [breakpoints.up('md')]: {
+      borderRadius: BORDER_RADIUS,
+    },
+  },
+  cardHeader: {
+    borderBottom: BORDER,
+  },
+}));
 
 const StarredPage: NextPage = () => {
+  const classes = useStyles();
   const { t } = useTranslation();
   const { query } = useRouter();
+  const { isTabletOrDesktop } = useMediaQueries();
   const { tabsProps, leftTabPanelProps, rightTabPanelProps } = useTabs();
   const variables = R.pick(['page', 'pageSize'], query);
   const context = useLanguageHeaderContext();
@@ -80,6 +98,10 @@ const StarredPage: NextPage = () => {
     ? renderResourceTable
     : renderResourcesNotFound;
 
+  const renderHeader = isTabletOrDesktop && (
+    <CardHeader className={classes.cardHeader} title={t('starred:header')} />
+  );
+
   const renderTabs = (
     <>
       <Tabs {...tabsProps}>
@@ -96,10 +118,9 @@ const StarredPage: NextPage = () => {
       title: t('starred:title'),
       description: t('starred:description'),
     },
-    header: t('starred:header'),
-    disablePadding: true,
     topNavbarProps: {
       dynamicBackUrl: true,
+      header: t('starred:header'),
     },
   };
 
@@ -112,7 +133,14 @@ const StarredPage: NextPage = () => {
   }
 
   if (userMe) {
-    return <SettingsTemplate {...layoutProps}>{renderTabs}</SettingsTemplate>;
+    return (
+      <MainTemplate {...layoutProps}>
+        <Paper className={classes.root}>
+          {renderHeader}
+          {renderTabs}
+        </Paper>
+      </MainTemplate>
+    );
   }
 
   return <NotFoundTemplate />;
