ac7c90b24f201be330e45ab1ae784cb5333a70a2
Markus Blomqvist <markus@skoleapp.com>
2020-12-28 10:58:03 +0200

    Remove the keywords meta tag for now

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -6,7 +6,6 @@ import { SeoProps } from 'types';
 export const HeadComponent: React.FC<SeoProps> = ({ title: customTitle, description }) => {
   const { palette } = useTheme();
   const title = customTitle ? `Skole | ${customTitle}` : 'Skole';
-  const keywords = 'study materials, distance learning, university'; // TODO: Optimize and translate these.
 
   const renderCommonMetaTags = (
     <>
@@ -17,7 +16,6 @@ export const HeadComponent: React.FC<SeoProps> = ({ title: customTitle, descript
         content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
       />
       <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
       <title>{title}</title>
     </>
   );
