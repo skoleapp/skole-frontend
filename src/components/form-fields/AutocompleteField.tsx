8d7e235d410ca7b779fbc40fab1823f4e264bd2a
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-02-06 22:11:48 +0200

    Show course codes in parenthesis in auto complete lists

--- src/components/form-fields/AutocompleteField.tsx
+++ src/components/form-fields/AutocompleteField.tsx
@@ -56,7 +56,7 @@ export const AutocompleteField: React.FC<Props & TextFieldProps> = ({
     });
 
     if (suffix) {
-      label = `${label} - ${suffix}`;
+      label = `${label} (${suffix})`;
     }
 
     return label;
