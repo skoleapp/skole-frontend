3079620e7f8f5225329b49db5d36c71331d1165c
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-02-11 14:49:34 +0200

    Remove unused import from `CourseTableRow`.

--- src/components/tables/CourseTableRow.tsx
+++ src/components/tables/CourseTableRow.tsx
@@ -9,7 +9,6 @@ import { CourseObjectType } from 'generated';
 import { useDayjs, useMediaQueries } from 'hooks';
 import { useTranslation } from 'lib';
 import Link from 'next/link';
-import * as R from 'ramda';
 import React from 'react';
 import { BORDER } from 'styles';
 import { ColSpan, TableRowProps } from 'types';
