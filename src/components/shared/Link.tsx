8ae08fbbde8923ba8e711ad032e02a550e946a35
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-02-13 15:42:48 +0200

    Fix dynamic with for link buttons

--- src/components/shared/Link.tsx
+++ src/components/shared/Link.tsx
@@ -1,21 +1,31 @@
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import NextLink, { LinkProps } from 'next/link';
 import React from 'react';
-
-type Props = LinkProps;
 
 const useStyles = makeStyles(() => ({
   link: {
     textDecoration: 'none',
     color: 'inherit',
   },
+  fullWidth: {
+    width: '100%',
+  },
 }));
 
-export const Link: React.FC<Props> = ({ href, children, ...props }) => {
+interface Props extends LinkProps {
+  fullWidth?: boolean;
+}
+
+export const Link: React.FC<Props> = ({ href, children, fullWidth, ...props }) => {
   const classes = useStyles();
+
   return (
-    <NextLink href={href} {...props}>
-      <a className={classes.link} href={String(href)}>
+    <NextLink href={href} {...props} passHref>
+      <a // eslint-disable-line jsx-a11y/anchor-is-valid
+        // Ignore: the `passHref` automatically passes the href to the anchor tag.
+        className={clsx(classes.link, fullWidth && classes.fullWidth)}
+      >
         {children}
       </a>
     </NextLink>
