fd552fb4a4da6d116b38fb695d2b21862ab7beac
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-02-18 09:10:49 +0200

    Fix text overflow for all text links

--- src/components/shared/TextLink.tsx
+++ src/components/shared/TextLink.tsx
@@ -1,11 +1,10 @@
 import MaterialLink, { LinkProps as MaterialLinkProps } from '@material-ui/core/Link';
-import { LinkProps } from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import React from 'react';
-
-import { Link } from './Link';
 
 type Props = LinkProps & Omit<MaterialLinkProps, 'href'>;
 
+// `next/link` is being used here on purpose as the `MaterialLink` is already an anchor tag.
 export const TextLink: React.FC<Props> = ({ href, children, ...props }) => (
   <Link href={href} passHref>
     <MaterialLink {...props}>{children}</MaterialLink>
