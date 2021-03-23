41a1c83ac250f7593511b055f968509441f4b2f3
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-03-23 11:59:04 +0200

    Remove the desktop navigation

--- src/styles.ts
+++ src/styles.ts
@@ -7,7 +7,6 @@ export const BORDER = `0.05rem solid ${grey[300]}`;
 export const BOTTOM_NAVBAR_HEIGHT = '3.25rem';
 export const TOP_NAVBAR_HEIGHT_MOBILE = '3.25rem';
 export const TOP_NAVBAR_HEIGHT_DESKTOP = '4rem';
-export const TOP_NAVBAR_HEIGHT_WITH_DESKTOP_NAVIGATION = '7rem';
 
 export const COLORS = {
   primary: '#ad3636',
