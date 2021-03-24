9b87eeb0f0d5f5b08eb4e2be481b1c5f8f3baf93
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-03-24 16:27:28 +0200

    Create landing page 2.0
    Also remove background from landing page and home pages.

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,6 +2,5 @@ export { AttachmentViewer } from './AttachmentViewer';
 export { BottomNavbar } from './BottomNavbar';
 export { Footer } from './Footer';
 export { HeadComponent as Head } from './Head';
-export { MainBackground } from './MainBackground';
 export { Notifications } from './Notifications';
 export { TopNavbar } from './TopNavbar';
