a536f3c7945b8d0aa194d5cb01d3fed616d60f65
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-04-18 21:12:01 +0300

    Fix bug of initial values not working for switch form fields in account settings

--- src/components/form-fields/SwitchFormField.tsx
+++ src/components/form-fields/SwitchFormField.tsx
@@ -15,7 +15,7 @@ interface Props {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const SwitchFormField: React.FC<Props> = ({ field, form, ...props }) => (
   <FormControl>
-    <FormControlLabel {...field} {...props} control={<Switch checked={field.value} />} />
+    <FormControlLabel {...field} {...props} control={<Switch checked={!!field.value} />} />
     <ErrorMessage name={field.name} component={FormErrorMessage} />
   </FormControl>
 );
