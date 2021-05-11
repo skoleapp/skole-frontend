0d47f67e5983017c78dbdfafe4ab3e6aa49945f7
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-05-11 20:16:01 +0300

    Potentially fix e2e test for logout page

--- cypress/integration/logout.test.ts
+++ cypress/integration/logout.test.ts
@@ -3,11 +3,12 @@ import { urls } from 'utils';
 describe('Logout Page Test Suite', () => {
   beforeEach(() => {
     cy.graphQlMock({ operationName: 'UserMe', fixture: 'userMe' }).as('userMe');
+    cy.graphQlMock({ operationName: 'GraphQLLogout', fixture: 'logout' }).as('logout');
   });
 
   it('renders', () => {
     cy.visit(urls.logout);
-    cy.wait('@userMe');
+    cy.wait(['@userMe', '@logout']);
     cy.url().should('include', urls.logout);
   });
 });
